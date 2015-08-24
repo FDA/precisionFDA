d33a9571d593c728102b103911cef7e521384f2f
Evan Worley <evanworley@gmail.com>
2015-08-24 16:53:45 -0700

    Removing source command, only needed for docker

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -3,7 +3,6 @@
 echo "++++ Setting up RVM"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
-source /etc/profile.d/rvm.sh
 
 echo "++++ Installing ruby 2.2.1"
 rvm install ruby-2.2.1
