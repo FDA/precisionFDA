98ade4259f04b59daf539e8fe321a1b531f28003
Fahd Butt <fbutt@dnanexus.com>
2015-09-01 18:16:29 -0700

    Building out templates and controllers for list views
    - mock data for the 4 main tabs
    - partials for grid and toolbar

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,4 +1,59 @@
 class FilesController < ApplicationController
   def index
+    @toolbar = {
+      fixed: [
+        {icon: "fa fa-plus-square fa-fw", label: "Add Files", link: "#"}
+      ]
+    }
+
+    @grid = {
+      header: [
+        {field: "name", display: "Name"},
+        {field: "size", display: "Size"},
+        {field: "created", display: "Created"},
+        {field: "addedBy", display: "Added by"}
+      ],
+      rows: [
+        [
+          {
+            field: "name",
+            display: "File A",
+            icon: "fa fa-lock fa-fw",
+            link: "#"
+          },
+          {
+            field: "size",
+            display: "10 GB"
+          },
+          {
+            field: "created",
+            display: "8/15/2015"
+          },
+          {
+            field: "addedBy",
+            display: "Doogie Howser"
+          }
+        ],
+        [
+          {
+            field: "name",
+            display: "File B",
+            link: "#"
+          },
+          {
+            field: "size",
+            display: "5 GB"
+          },
+          {
+            field: "created",
+            display: "6/8/2015"
+          },
+          {
+            field: "addedBy",
+            display: "Doogie Howser"
+          }
+        ]
+      ]
+    }
   end
 end
