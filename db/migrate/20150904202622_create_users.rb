7f9541ab9abefea30ea6cc8ca06d5542e3206c38
Evan Worley <evanworley@gmail.com>
2015-09-14 17:54:36 -0700

    Adding more strict db types and default values

--- db/migrate/20150904202622_create_users.rb
+++ db/migrate/20150904202622_create_users.rb
@@ -1,18 +1,19 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :dxuser
-      t.string :private_files_project
-      t.string :public_files_project
-      t.string :private_comparisons_project
-      t.string :public_comparisons_project
-      t.integer :open_files_count
-      t.integer :closing_files_count
-      t.integer :pending_comparisons_count
-      t.integer :schema_version
+      t.string :dxuser, required: true
+      t.string :private_files_project, required: true
+      t.string :public_files_project, required: true
+      t.string :private_comparisons_project, required: true
+      t.string :public_comparisons_project, required: true
+      t.integer :open_files_count, default: 0
+      t.integer :closing_files_count, default: 0
+      t.integer :pending_comparisons_count, default: 0
+      t.integer :schema_version, default: 1
 
       t.timestamps null: false
     end
+
     add_index :users, :dxuser, unique: true
   end
 end
