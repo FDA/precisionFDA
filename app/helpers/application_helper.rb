40e931f692a648f76bf7998f9ebda3e0fcc157b6
Fahd Butt <fbutt@dnanexus.com>
2015-09-18 13:27:49 -0700

    Update flash message styles to use Bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@ module ApplicationHelper
   def page_title(separator = " – ")
     [content_for(:title), 'precisionFDA'].compact.join(separator)
   end
+
+  def bootstrap_class_for flash_type
+    case flash_type
+    when "success"
+        "alert-success"
+      when "error"
+        "alert-danger"
+      when "alert"
+        "alert-warning"
+      when "notice"
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
 end
