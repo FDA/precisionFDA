6546c877998e1b95397696b30d11e6d73895a8cd
George Asimenos <george+github@dnanexus.com>
2015-09-24 01:16:28 -0700

    Adding default parent when uploading files, adding that also to the migration

--- db/migrate/20150923234847_add_parent_to_user_file.rb
+++ db/migrate/20150923234847_add_parent_to_user_file.rb
@@ -1,5 +1,7 @@
 class AddParentToUserFile < ActiveRecord::Migration
   def change
     add_reference :user_files, :parent, index: true, polymorphic: true
+
+    UserFile.unscoped.select { |u| u.parent.nil? }.each { |u| u.update!(parent: User.find(u.user_id) ) }
   end
 end
