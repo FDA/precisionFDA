34aeaa17b31c917e77524c2813999732c8645da7
Fahd Butt <fbutt@dnanexus.com>
2015-09-25 14:42:15 -0700

    Summary of Comparisons (sans chart)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,14 @@ module ApplicationHelper
         flash_type.to_s
     end
   end
+
+  def humanizeSeconds secs
+    secs = secs.to_i
+    [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
+      if secs > 0
+        secs, n = secs.divmod(count)
+        "#{n.to_i} #{name}"
+      end
+    }.compact.reverse.join(' ')
+  end
 end
