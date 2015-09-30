0eb3550032724853ac4d97fd1c3c44b9b740cc0b
Fahd Butt <fbutt@dnanexus.com>
2015-09-30 00:23:52 -0700

    Notes hacked up - please setup controller variables for your demo

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -19,12 +19,12 @@ class NotesController < ApplicationController
           {field: "addedBy", display: "George Asimenos", link: user_path("george.fdauser")}
         ],
         [
-          {field: "name", display: "Studying NA12878 from an Illumina X Ten via the precisionFDA lens", link: note_path(1)},
+          {field: "name", display: "Studying NA12878 from an Illumina X Ten via the precisionFDA lens", link: note_path(2)},
           {field: "created", display: "9/16/2015"},
           {field: "addedBy", display: "George Asimenos", link: user_path("george.fdauser")}
         ],
         [
-          {field: "name", display: "How related are NA12878 and NA12877?", link: note_path(1)},
+          {field: "name", display: "How related are NA12878 and NA12877?", link: note_path(3)},
           {field: "created", display: "9/15/2015"},
           {field: "addedBy", display: "George Asimenos", link: user_path("george.fdauser")}
         ]
@@ -33,5 +33,44 @@ class NotesController < ApplicationController
   end
 
   def show
+     @toolbar = {
+      fixed: [
+        {label: "Edit", link: "#"},
+        {label: "Publish", link: "#"}
+      ]
+    }
+
+    if params[:id].to_i == 1
+      @name = "PrecisionFDA Benchmark VCFs"
+
+      comparisons = Comparison.accessible_by(@context.user_id)
+      # NOTE: Get benchmark vcfs
+      # @files = UserFile.accessible_by(@context.user_id)
+      # @files_grid = initialize_grid(@files, {
+      #   order: 'name',
+      #   order_direction: 'asc',
+      # })
+    elsif params[:id].to_i == 2
+      @name = "Studying NA12878 from an Illumina X Ten via the precisionFDA lens"
+
+      @comparisons = Comparison.accessible_by(@context.user_id)
+      @comparisons_grid = initialize_grid(@comparisons, {
+        order: 'name',
+        order_direction: 'asc',
+        conditions: {id: 1}
+      })
+
+      @comparison = @comparisons.take
+      @meta = ActiveSupport::JSON.decode(@comparison.meta)
+      @test_vcf = @comparison.input('test_vcf').user_file
+      @ref_vcf = @comparison.input('ref_vcf').user_file
+
+      # NOTE: Make this be a query for both INPUT and OUTPUT UserFiles
+      @files = @comparison.outputs
+      @files_grid = initialize_grid(@files, {
+        order: 'name',
+        order_direction: 'asc',
+      })
+    end
   end
 end
