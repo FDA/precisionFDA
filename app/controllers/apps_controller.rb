0bedb7d125bba997551bf7be12b3b11dede51eba
George Asimenos <george+github@dnanexus.com>
2015-09-29 19:15:07 -0700

    Further refine mockup data for apps

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -6,10 +6,10 @@ class AppsController < ApplicationController
       community: [
         {name:"bwa", display: "BWA", link: appjobs_path("bwa")},
         {name:"gatk", display: "GATK", link: appjobs_path("gatk")},
-        {name:"art", display: "ART FASTQ Generator", link: appjobs_path("art")}
+        {name:"art", display: "ART Illumina Read Simulation", link: appjobs_path("art")}
       ],
       custom: [
-        {name:"mycustom", display: "My Custom App", link: appjobs_path("mycustom")}
+        {name:"mycustom", display: "HugeSeq Pipeline", link: appjobs_path("mycustom")}
       ],
       active: app
     }
@@ -32,29 +32,29 @@ class AppsController < ApplicationController
         {field: "name", display: "Name"},
         {field: "duration", display: "Duration"},
         {field: "created", display: "Created"},
-        {field: "addedBy", display: "Added by"}
+        {field: "addedBy", display: "Launched by"}
       ],
       rows: [
         [
           {field: "state", display: "Running", classes: "state-running"},
-          {field: "name", display: "Job XYZ", link: "#"},
-          {field: "duration", display: "5 hrs"},
-          {field: "created", display: "5/15/2015"},
-          {field: "addedBy", display: "George Asimenos", link: "#"}
+          {field: "name", display: "ART whole-genome 150bp", link: "#"},
+          {field: "duration", display: "5 min"},
+          {field: "created", display: "9/17/2015"},
+          {field: "addedBy", display: "george.fdauser", link: "#"}
         ],
         [
           {field: "state", display: "Done", classes: "state-done"},
-          {field: "name", display: "Job ABC", link: "#"},
-          {field: "duration", display: "2 hrs"},
-          {field: "created", display: "2/15/2015"},
-          {field: "addedBy", display: "George Asimenos", link: "#"}
+          {field: "name", display: "ART HiSeq 1000 simulation", link: "#"},
+          {field: "duration", display: "20 min"},
+          {field: "created", display: "9/16/2015"},
+          {field: "addedBy", display: "george.fdauser", link: "#"}
         ],
         [
           {field: "state", display: "Failed", classes: "state-failed"},
-          {field: "name", display: "Job PKR", link: "#"},
-          {field: "duration", display: "5 min"},
-          {field: "created", display: "1/15/2015"},
-          {field: "addedBy", display: "George Asimenos", link: "#"}
+          {field: "name", display: "ART HiSeq 1000 simulation", link: "#"},
+          {field: "duration", display: "1 min"},
+          {field: "created", display: "9/15/2015"},
+          {field: "addedBy", display: "george.fdauser", link: "#"}
         ]
       ]
     }
