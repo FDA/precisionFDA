ad573fcbd798b2b3caad2860cda37c99318ee7db
George Asimenos <george+github@dnanexus.com>
2015-10-11 14:56:47 -0700

    Running "annotate" on notes

--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  slug       :string
+#  content    :text
+#  public     :boolean
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
