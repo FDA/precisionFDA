8ae49779da98fe4d3dbf5ee6668a4b57f20e7d90
Evan Worley <evanworley@gmail.com>
2015-10-15 18:46:32 -0700

    Landing opsworks integration/production deployment changes
    - Extracting client ID into environment based global
    - Fixing OAUTH2 returnTo to be ENV specific
    - Adding chef script to precompile assets
    - Adding rake task to extend the asset pipeline and copy font awesome
      without a digest
    - Fixing up production config
    - using unicorn as the application server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,8 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  #config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -29,10 +30,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
+  config.assets.debug = false
   config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
