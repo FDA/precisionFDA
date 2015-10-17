9b74bdf42391ba39b340fdbead8ef459607f3a96
Fahd Butt <fbutt@dnanexus.com>
2015-10-16 18:03:43 -0700

    Comparison states - hide metadata if not done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,11 +20,15 @@ module ApplicationHelper
 
   def humanizeSeconds secs
     secs = secs.to_i
-    [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
-      if secs > 0
-        secs, n = secs.divmod(count)
-        "#{n.to_i} #{name}"
-      end
-    }.compact.reverse.join(' ')
+    if secs == 0
+      return "N/A"
+    else
+      [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
+        if secs > 0
+          secs, n = secs.divmod(count)
+          "#{n.to_i} #{name}"
+        end
+      }.compact.reverse.join(' ')
+    end
   end
 end
