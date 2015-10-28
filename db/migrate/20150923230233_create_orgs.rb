438d1cc0eeb09733e8f1d5f69bd96cb156e3fc54
George Asimenos <george+github@dnanexus.com>
2015-10-28 15:26:05 -0700

    Migration fix for foreign key in orgs

--- db/migrate/20150923230233_create_orgs.rb
+++ db/migrate/20150923230233_create_orgs.rb
@@ -3,10 +3,11 @@ class CreateOrgs < ActiveRecord::Migration
     create_table :orgs do |t|
       t.string :handle, required: true
       t.string :name
-      t.references :admin, index: true, foreign_key: true, required: true
+      t.references :admin, index: true, required: true
 
       t.timestamps null: false
     end
     add_index :orgs, :handle, unique: true
+    add_foreign_key :orgs, :users, column: 'admin_id'
   end
 end
