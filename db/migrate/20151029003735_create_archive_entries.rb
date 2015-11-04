8fbdd8386aba2f1fe3a8794e8ca3798140897330
Evan Worley <evanworley@gmail.com>
2015-11-03 18:48:01 -0800

    Fixing up migration to work with mysql

--- db/migrate/20151029003735_create_archive_entries.rb
+++ db/migrate/20151029003735_create_archive_entries.rb
@@ -3,8 +3,9 @@ class CreateArchiveEntries < ActiveRecord::Migration
     create_table :archive_entries do |t|
       t.text :path
       t.string :name
-      t.references :asset, index: true, foreign_key: true
+      t.references :asset, index: true
     end
     add_index :archive_entries, :name
+    add_foreign_key :archive_entries, :user_files, column: 'asset_id'
   end
 end
