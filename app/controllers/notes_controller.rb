9a1478065a56de1cdaa5187f4e9c0a2bb67d3a73
Fahd Butt <fbutt@dnanexus.com>
2015-11-09 04:44:50 -0800

    Deleting attachments from notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,18 +9,18 @@ class NotesController < ApplicationController
     if request.path != note_path(@note)
       redirect_to @note
     else
-      @comparisons = @note.comparisons.accessible_by(@context)
-      @files = @note.real_files.accessible_by(@context)
-      @apps = @note.apps.accessible_by(@context)
-      @jobs = @note.jobs
+      comparisons = @note.comparisons.accessible_by(@context)
+      files = @note.real_files.accessible_by(@context)
+      apps = @note.apps.accessible_by(@context)
+      jobs = @note.jobs
 
-      if @note[:user_id] == @context.user_id
-        js note: @note.slice(:id, :content, :title),
-           comparisons: (@comparisons.map { |o| o.slice(:id, :name, :stats)}),
-           files: (@files.map { |o| o.slice(:dxid, :name)}),
-           apps: (@apps.map { |o| o.slice(:dxid, :title)}),
-           jobs: (@jobs.map { |o| o.slice(:dxid, :name)})
-      end
+      attachments = {
+        comparisons: (comparisons.map { |o| o.slice(:id, :name, :stats)}),
+        files: (files.map { |o| o.slice(:id, :dxid, :name)}),
+        apps: (apps.map { |o| o.slice(:id, :dxid, :title)}),
+        jobs: (jobs.map { |o| o.slice(:id, :dxid, :name)})
+      }
+      js note: @note.slice(:id, :content, :title), attachments: attachments
     end
   end
 
@@ -36,10 +36,22 @@ class NotesController < ApplicationController
   end
 
   def update
+    id = params[:id].to_i
+    raise unless id.is_a?(Integer)
+
+    attachments = params[:attachments]
+    # raise unless attachments.is_a?(Array)
+
     updated = false
     note = Note.find(params[:id])
     if note[:user_id] == @context.user_id
       Note.transaction do
+        note.attachments.destroy_all
+        attachments.each do |attachment|
+          # NOTE: I'm not sure why its send attachments in such a format
+          note.attachments.find_or_create_by(item_id: attachment[1][:id].to_i, item_type: attachment[1][:type])
+        end
+
         params = note_params()
         if note.update!(params)
           updated = true
