0cdd3033021b0db7a02bf7198b4c9d1df08a8520
asimenos <george@dnanexus.com>
2015-11-10 13:33:17 -0800

    Adding the FDA as a recipient to the invitation email

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -8,7 +8,7 @@ class NotificationsMailer < ApplicationMailer
   def invitation_email(invitation)
     @invitation = invitation
 
-    mail to: ["precisionfda-support@dnanexus.com"],
+    mail to: ["precisionfda-support@dnanexus.com", "precisionfda@fda.hhs.gov"],
          reply_to: "precisionfda-support@dnanexus.com",
          subject: "New access request from #{invitation.first_name} #{invitation.last_name}"
   end
