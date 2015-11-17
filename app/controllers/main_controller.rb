08c7bd50582c6ac168b853aeb011c28939078e63
George Asimenos <george+github@dnanexus.com>
2015-11-17 12:51:09 -0800

    Request form updates

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -84,6 +84,8 @@ class MainController < ApplicationController
     if request.post?
       p = params.require(:invitation).permit(:first_name, :last_name, :email, :org, :duns, :address, :phone, :singular, :req_reason, :req_data, :req_software, :research_intent, :clinical_intent, :humanizer_answer, :humanizer_question_id)
       p[:ip] = request.remote_ip.to_s
+      p[:research_intent] = (p[:research_intent] == "1")
+      p[:clinical_intent] = (p[:clinical_intent] == "1")
       AUDIT_LOGGER.info("Access requested: #{p.to_json}")
       Invitation.transaction do
         @invitation = Invitation.create(p)
