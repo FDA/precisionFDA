a9037f9ed01d780a72d9ec6dfa57625bbe8e7733
George Asimenos <george+github@dnanexus.com>
2015-12-16 15:08:10 -0800

    Changing reporting to add more information

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -270,7 +270,7 @@ class ProfileController < ApplicationController
       p.use_autowidth = true
       Time.use_zone ActiveSupport::TimeZone.new('America/New_York') do
         p.workbook.add_worksheet(:name => "Users") do |sheet|
-          sheet.add_row ["", "", "username", "first name", "last name", "email", "provisioned at", "last login"]
+          sheet.add_row ["", "", "username", "first name", "last name", "email", "provisioned at", "last login", "bytes stored", "apps count", "jobs count"]
           Org.order(:name).all.each do |org|
             ["name", "handle", "address", "phone"].each do |label|
               sheet.add_row ["Organization #{label}:", org.send(label)]
@@ -278,16 +278,22 @@ class ProfileController < ApplicationController
             users = [org.admin] + org.users.order(:dxuser).all.reject { |u| u.id == org.admin_id }
             users.each do |user|
               role = user.id == org.admin_id ? "Admin:" : "Member:"
-              sheet.add_row [role, "", user.dxuser, user.first_name, user.last_name, user.email, user.created_at.strftime("%Y-%m-%d %H:%M"), user.last_login ? user.last_login.strftime("%Y-%m-%d %H:%M") : ""]
+              sheet.add_row [role, "", user.dxuser, user.first_name, user.last_name, user.email, user.created_at.strftime("%Y-%m-%d %H:%M"), user.last_login ? user.last_login.strftime("%Y-%m-%d %H:%M") : "", user.user_files.sum(:file_size), user.app_series.count, user.jobs.count]
             end
             sheet.add_row
           end
         end
         p.workbook.add_worksheet(:name => "Requests") do |sheet|
-          sheet.add_row ["time", "first name", "last name", "email", "organization", "self-represent?", "address", "phone", "duns", "research?", "clinical?", "has data?", "has software?", "reason" ]
-          Invitation.find_each do |inv|
+          sheet.add_row ["time", "in_system?", "first name", "last name", "email", "organization", "self-represent?", "address", "phone", "duns", "research?", "clinical?", "has data?", "has software?", "reason" ]
+          Invitation.includes(:user).find_each do |inv|
             row = []
             row << inv.created_at.strftime("%Y-%m-%d %H:%M")
+            if inv.user.present?
+              row << inv.user.dxuser
+            else
+              u = User.where.any_of({first_name: inv.first_name, last_name: inv.last_name}, {normalized_email: inv.email.downcase.strip}).take
+              row << (u ? "maybe #{u.dxuser}" : "")
+            end
             row += [inv.first_name, inv.last_name, inv.email, inv.org, inv.singular, inv.address, inv.phone, inv.duns, inv.research_intent, inv.clinical_intent, inv.req_data, inv.req_software, inv.req_reason]
             sheet.add_row row
           end
