8ec365a1d0964658c2e9779c11db8175c8efae02
George Asimenos <george+github@dnanexus.com>
2016-02-18 15:32:17 -0800

    Challenge updates

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,9 +1,9 @@
 class ChallengesController < ApplicationController
-  skip_before_action :require_login, {only: [:reproducibility_and_accuracy]}
+  skip_before_action :require_login, {only: [:consistency]}
 
   skip_before_action :require_login,     only: []
   before_action :require_login_or_guest, only: []
 
-  def reproducibility_and_accuracy
+  def consistency
   end
 end
