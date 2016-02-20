bd32ad0218b7c805af27f58e39204f10c5eeb1ac
Fahd Butt <fbutt@dnanexus.com>
2016-02-19 16:00:46 -0800

    Challenge page updates - improving detail text and CTA button

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -5,5 +5,26 @@ class ChallengesController < ApplicationController
   before_action :require_login_or_guest, only: []
 
   def consistency
+    @discussion = {
+      id: 1
+    }
+
+    @challenge = {
+      active: DateTime.now.in_time_zone < DateTime.new(2016,4,25).in_time_zone.end_of_day,
+      joined: false
+    }
+  end
+
+  def join
+    discussion_id = params[:id]
+
+    if @context.logged_in?
+      # if not following discussion_id
+        # follow discussion_id
+      # redirect_to discussion
+      redirect_to request_access_path
+    else
+      redirect_to request_access_path
+    end
   end
 end
