607129cea08e796b3f9858d56e7e20783c52ed0c
Bill Agee <billagee@gmail.com>
2016-03-21 13:51:26 -0700

    Re-ran 'rails g test_unit:controller <ctlrs>'
    Also removed deleted columns from fixtures.

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,29 +1,7 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit
-    assert_response :success
-  end
-
-  test "should get update" do
-    get :update
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
