4dd4a06d0a152be5aba676ce356c9ebc5229b7fd
Fahd Butt <fbutt@dnanexus.com>
2016-03-28 16:19:17 -0700

    For staging - must be admin of either precisionfda or dnanexus org to be a superadmin

--- app/models/user.rb
+++ app/models/user.rb
@@ -99,7 +99,7 @@ class User < ActiveRecord::Base
     if Rails.env.production? && ENV["DNANEXUS_BACKEND"] == "production"
       dxuser == "elaine.johanson" || dxuser == "ruth.bandler"
     else
-      org.handle == "precisionfda" || org.handle == "dnanexus"
+      (org.handle == "precisionfda" || org.handle == "dnanexus") && org.admin_id == id
     end
   end
 
