5b8c0dc25c9b40f5d0b316c3fa527030e6a2abfb
George Asimenos <george+github@dnanexus.com>
2016-04-25 14:58:50 -0700

    Renaming giab -> truth

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,10 +1,10 @@
 class ChallengesController < ApplicationController
-  skip_before_action :require_login, {only: [:index, :consistency, :giab, :join]}
+  skip_before_action :require_login, {only: [:index, :consistency, :truth, :join]}
   before_action :require_login_or_guest, only: []
 
   def index
     # TODO: Update when we have a challenge index page
-    redirect_to giab_challenges_path
+    redirect_to truth_challenges_path
   end
 
   # Challenge 1 - Consistency
@@ -28,15 +28,15 @@ class ChallengesController < ApplicationController
     end
   end
 
-  # Challenge 2 - Genome-in-a-Bottle
-  def giab
-    @discussion = Discussion.find_by(id: GIAB_DISCUSSION_ID)
+  # Challenge 2 - Truth
+  def truth
+    @discussion = Discussion.find_by(id: TRUTH_DISCUSSION_ID)
 
-    @challengedEndDate = GIAB_CHALLENGE_END_DATE
+    @challengedEndDate = TRUTH_CHALLENGE_END_DATE
 
     @challenge = {
       launched: !@discussion.nil? && @discussion.public?,
-      active: GIAB_CHALLENGE_ACTIVE,
+      active: TRUTH_CHALLENGE_ACTIVE,
       joined: @context.logged_in? && !@discussion.nil? && @discussion.followed_by?(@context.user)
     }
 
@@ -54,7 +54,7 @@ class ChallengesController < ApplicationController
       discussion = Discussion.accessible_by(@context).find(params[:id])
       if !discussion.followed_by?(@context.user)
         @context.user.follow(discussion)
-        flash[:success] = "You have joined the challenge! Submit and publish your response by the deadline."
+        flash[:success] = "You have joined the challenge! Fill in and publish your response by the deadline."
       end
       redirect_to discussion_path(discussion)
     else
