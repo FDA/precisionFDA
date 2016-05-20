dc6edc3d0ba0e812b49a1fe26e7a0751ca807984
Fahd Butt <fbutt@dnanexus.com>
2016-05-20 16:14:44 -0700

    Recognition and medal badges

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -6,4 +6,40 @@ module ChallengesHelper
   def pct(n)
     sprintf("%.2f%%", n*100.0)
   end
+
+  def medal_badge(medal)
+    if medal != ''
+      raw """
+        <div class='challenge-badge challenge-badge-medal'>
+          <span class='fa-stack fa-lg'>
+            <i class='fa fa-certificate fa-stack-2x'></i>
+            <i class='fa fa-trophy fa-stack-1x fa-inverse'></i>
+          </span>
+          #{medal}
+        </div>
+      """
+    end
+  end
+
+  def recognition_badge(recognition)
+    if recognition != ''
+      if recognition.include? "highest"
+        raw """
+          <div class='challenge-badge challenge-badge-recognition-highest'>
+            <span class='fa fa-certificate'></span>
+            #{recognition}
+          </div>
+        """
+      elsif recognition.include? "not-considered"
+        return "Not Considered"
+      else
+        raw """
+          <div class='challenge-badge challenge-badge-recognition'>
+            <span class='fa fa-certificate'></span>
+            #{recognition}
+          </div>
+        """
+      end
+    end
+  end
 end
