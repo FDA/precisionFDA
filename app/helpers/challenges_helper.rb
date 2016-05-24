3e00626f8fa812a4c92f13fe3dfbad1535e37428
Fahd Butt <fbutt@dnanexus.com>
2016-05-24 11:23:04 -0700

    Update challenge awards section

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -7,17 +7,36 @@ module ChallengesHelper
     sprintf("%.2f%%", n*100.0)
   end
 
-  def medal_badge(medal, type = '')
+  def medal_badge(medal, qualifier = '', type = '')
     if medal != ''
-      raw """
-        <div class='challenge-badge challenge-badge-medal #{type == "large" ? "challenge-badge-lg": ""}'>
-          <span class='fa-stack #{type == "large" ? "fa-lg": ""}' aria-hidden='true'>
-            <i class='fa fa-certificate fa-stack-2x'></i>
-            <i class='fa fa-trophy fa-stack-1x fa-inverse'></i>
-          </span>
-          #{medal}
-        </div>
-      """
+      if type == 'large'
+        raw """
+          <div class='challenge-badge challenge-badge-medal challenge-badge-lg'>
+            <div class='media'>
+              <div class='media-left'>
+                <span class='fa-stack fa-lg' aria-hidden='true'>
+                  <i class='fa fa-certificate fa-stack-2x'></i>
+                  <i class='fa fa-trophy fa-stack-1x fa-inverse'></i>
+                </span>
+              </div>
+              <div class='media-body'>
+                <div class='challenge-badge-qualifier'>#{qualifier}</div>
+                <div class='challenge-badge-medal-name'>#{medal}</div>
+              </div>
+            </div>
+          </div>
+        """
+      else
+        raw """
+          <div class='challenge-badge challenge-badge-medal'>
+            <span class='fa-stack' aria-hidden='true'>
+              <i class='fa fa-certificate fa-stack-2x'></i>
+              <i class='fa fa-trophy fa-stack-1x fa-inverse'></i>
+            </span>
+            #{medal}
+          </div>
+        """
+      end
     end
   end
 
