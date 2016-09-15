a235f6ed9ae8acc2047a2de0456508e25b7a3e78
Fahd Butt <fbutt@dnanexus.com>
2016-09-15 10:27:25 -0700

    Get meta_appathon apps from appathon instead of assuming time range

--- app/models/meta_appathon.rb
+++ app/models/meta_appathon.rb
@@ -54,10 +54,9 @@ class MetaAppathon < ActiveRecord::Base
   end
 
   def apps
-    # FIXME: Could the sorting be made more efficient?
-    _apps = AppSeries.accessible_by_public.where(user_id: member_ids).where("created_at > ?", start_at).where("created_at < ?", end_at)
-    _apps = _apps.map { |s| s.latest_version_app }.reject(&:nil?)
-    return _apps.sort_by {|a| [-a.app_series.get_upvotes.size, a.app_series.name] }
+    _apps = []
+    appathons.each {|appathon| _apps << appathon.apps}
+    _apps.flatten
   end
 
   def self.editable_by(context)
