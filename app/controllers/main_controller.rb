e407315d24a8c3cefd64f9d4eebafe02a43de66d
George Asimenos <george+github@dnanexus.com>
2016-09-27 11:50:24 -0700

    Fixes for login

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -198,32 +198,52 @@ class MainController < ApplicationController
       AUDIT_LOGGER.info("User #{username} attempted to log in from an existing DNAnexus account")
       render "_partials/_error", status: 403, locals: {message: "ERROR: You cannot use an existing DNAnexus account (#{username}) to log into precisionFDA. You need to apply for and obtain a separate precisionFDA account."}
     else
-      User.transaction do
-        user.reload
-        if user.last_login.nil? && user.private_files_project.nil?
-          api = DNAnexusAPI.new(token)
-          # Private files
-          private_files_project = api.call("project", "new", {name: "precisionfda-personal-files-#{username}", billTo: user.billto})["id"]
-          # Private comparisons
-          private_comparisons_project = api.call("project", "new", {name: "precisionfda-personal-comparisons-#{username}", billTo: user.billto})["id"]
-          # Public files
-          public_files_project = api.call("project", "new", {name: "precisionfda-public-files-#{username}", billTo: user.billto})["id"]
-          api.call(public_files_project, "invite", {invitee: ORG_EVERYONE, level: "VIEW", suppressEmailNotification: true})
-          # Public comparisons
-          public_comparisons_project = api.call("project", "new", {name: "precisionfda-public-comparisons-#{username}", billTo: user.billto})["id"]
-          api.call(public_comparisons_project, "invite", {invitee: ORG_EVERYONE, level: "VIEW", suppressEmailNotification: true})
-          # User settings
-          api.call(full_username, "update", {policies: {emailWhenJobComplete: "never"}})
+      if user.last_login.nil? && user.private_files_project.nil?
+        AUDIT_LOGGER.info("User #{username} is attempting to log in for the first time; setting up projects (step 1 of 9)")
+        api = DNAnexusAPI.new(token)
 
-          user.private_files_project = private_files_project
-          user.private_comparisons_project = private_comparisons_project
-          user.public_files_project = public_files_project
-          user.public_comparisons_project = public_comparisons_project
+        # Private files
+        private_files_project = api.call("project", "new", {name: "precisionfda-personal-files-#{username}", billTo: user.billto})["id"]
+        AUDIT_LOGGER.info("User #{username} is attempting to log in for the first time; setting up projects (step 2 of 9)")
 
-          AUDIT_LOGGER.info("User #{username} logged in for the first time")
+        # Private comparisons
+        private_comparisons_project = api.call("project", "new", {name: "precisionfda-personal-comparisons-#{username}", billTo: user.billto})["id"]
+        AUDIT_LOGGER.info("User #{username} is attempting to log in for the first time; setting up projects (step 3 of 9)")
+
+        # Public files
+        public_files_project = api.call("project", "new", {name: "precisionfda-public-files-#{username}", billTo: user.billto})["id"]
+        AUDIT_LOGGER.info("User #{username} is attempting to log in for the first time; setting up projects (step 4 of 9)")
+        api.call(public_files_project, "invite", {invitee: ORG_EVERYONE, level: "VIEW", suppressEmailNotification: true})
+        AUDIT_LOGGER.info("User #{username} is attempting to log in for the first time; setting up projects (step 5 of 9)")
+
+        # Public comparisons
+        public_comparisons_project = api.call("project", "new", {name: "precisionfda-public-comparisons-#{username}", billTo: user.billto})["id"]
+        AUDIT_LOGGER.info("User #{username} is attempting to log in for the first time; setting up projects (step 6 of 9)")
+        api.call(public_comparisons_project, "invite", {invitee: ORG_EVERYONE, level: "VIEW", suppressEmailNotification: true})
+        AUDIT_LOGGER.info("User #{username} is attempting to log in for the first time; setting up projects (step 7 of 9)")
+
+        # User settings
+        api.call(full_username, "update", {policies: {emailWhenJobComplete: "never"}})
+        AUDIT_LOGGER.info("User #{username} is attempting to log in for the first time; setting up projects (step 8 of 9)")
+
+        User.transaction do
+          user.reload
+          if user.last_login.nil? && user.private_files_project.nil?
+            user.private_files_project = private_files_project
+            user.private_comparisons_project = private_comparisons_project
+            user.public_files_project = public_files_project
+            user.public_comparisons_project = public_comparisons_project
+            AUDIT_LOGGER.info("User #{username} is attempting to log in for the first time; setting up projects (step 9 of 9)")
+          end
+          user.last_login = Time.now
+          user.save!
         end
-        user.last_login = Time.now
-        user.save!
+      else
+        User.transaction do
+          user.reload
+          user.last_login = Time.now
+          user.save!
+        end
       end
       save_session(user.id, username, token, expiration_time, user.org_id)
       AUDIT_LOGGER.info("User #{username} logged in")
