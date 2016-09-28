ef8398e915530fa97955f7c10df4165789564b0b
Alan Zhu <azhu@dnanexus.com>
2016-09-28 12:27:23 -0700

    Improving transactions for comparisons *Temporary solution before overhauling comparisons object *Removed pending_comparisons_count

--- db/migrate/20150904202622_create_users.rb
+++ db/migrate/20150904202622_create_users.rb
@@ -8,7 +8,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :public_comparisons_project, required: true
       t.integer :open_files_count, default: 0
       t.integer :closing_files_count, default: 0
-      t.integer :pending_comparisons_count, default: 0
       t.integer :schema_version, required: true
 
       t.timestamps null: false
