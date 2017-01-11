e907cd06c74fa6b8121be7155979efad15a58581
George Asimenos <george+github@dnanexus.com>
2017-01-10 22:01:05 -0800

    Updating staging config to point to precisionfda-staging.dnanexus.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,11 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => "precision.fda.gov" }
+  if ENV["DNANEXUS_BACKEND"] == "production"
+    config.action_mailer.default_url_options = { :host => "precision.fda.gov" }
+  else
+    config.action_mailer.default_url_options = { :host => "precisionfda-staging.dnanexus.com" }
+  end
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
