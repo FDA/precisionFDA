7f4867d7c9a110d8dfb448365c2e286068f98317
Alan Zhu <azhu@dnanexus.com>
2017-03-12 19:43:10 -0700

    initial non-expert view of expert page

--- db/migrate/20170311021446_create_expert_questions.rb
+++ db/migrate/20170311021446_create_expert_questions.rb
@@ -2,6 +2,7 @@ class CreateExpertQuestions < ActiveRecord::Migration
   def change
     create_table :expert_questions do |t|
       t.references :expert, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.text   :body
       t.string :state, index: true
 
