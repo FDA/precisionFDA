5723772e143642bc87262a3877d09ae01ea0e7f5
aabramenko <aabramenko-c@dnanexus.com>
2017-12-13 20:56:18 +0300

    workflow-2

--- test/functional/src/main/java/precisionFDA/locators/WorkflowLocators.java
+++ test/functional/src/main/java/precisionFDA/locators/WorkflowLocators.java
@@ -20,5 +20,39 @@ public class WorkflowLocators {
 
     public static final String WORKFLOW_APP_BLOCK_LINK = "//a//*[contains(text(), '{APP_NAME}')][contains(@class, 'gear')]";
 
+    public static final String WORKFLOW_APP_SETTINGS_MODAL_INSTANCE_SELECTOR = "//*[@id='configure-stage-modal']//select[@name='instance_type']";
+
+    public static final String WORKFLOW_APP_SETTINGS_MODAL_FIRST_INPUT_REQUIRED = "//*[@id='configure-stage-modal']//*[contains(@class, 'padded')][1]//input[@type='checkbox']";
+
+    public static final String WORKFLOW_APP_SETTINGS_MODAL_CLOSE_BUTTON = "//*[@id='configure-stage-modal']//button[text()='Close']";
+
+    public static final String WORKFLOW_UPDATE_WF_BUTTON = "//button[text()='Update']";
+
+    public static final String WORKFLOW_CREATE_WF_BUTTON = "//button[text()='Create']";
+
+    public static final String CREATED_WF_DIAGRAM_LINK = "//a/*[text()='Workflow Diagram']/..";
+
+    public static final String CREATED_WF_APP_NAME_INPUT_TEMPLATE = "//*[text()='Inputs']/..//i[contains(text(), '{APP_NAME}')]";
+
+    public static final String RUN_WF_BUTTON = "//*[contains(text(), 'Run Workflow')][contains(@class, 'btn')]";
+
+    public static final String RUN_WF_CONFIG_ANALISYS_NAME_INPUT = "//label[text()='Analysis Name']/..//input";
+
+    public static final String LINK_TO_CREATED_WORKFLOW_TEMPLATE = "//a/*[text()='{WF_TITLE}']/..";
+
+    public static final String RUN_WF_CONFIG_APP_INPUT_TEMPLATE = "//*[text()='Inputs']/../..//*[text()='{APP_NAME}']";
+
+    public static final String RUN_WF_CONFIG_SELECT_FILE_BUTTON = "//button[contains(text(), 'Select file')]";
+
+    public static final String RUN_WF_CONFIG_MODAL_FILES_LINK = "//*[contains(@class, 'modal-content')]//li/a/*[text()='Files']/..";
+
+    public static final String RUN_WF_CONFIG_MODAL_APP_INPUT_FILE_TEMPLATE = "//*[contains(@class, 'modal-content')]//*[text()='{FILE_NAME}']";
+
+    public static final String RUN_WF_CONFIG_MODAL_SELECT_BUTTON = "//*[contains(@class, 'modal-content')]//button/*[text()='Select']/..";
+
+    public static final String RUN_WF_CONFIG_SELECTED_INPUT_FILE_TEMPLATE = "//*[text()='Inputs']/../..//button[text()='{FILE_NAME}']";
+
+    public static final String CREATED_WF_ANALYSES_WF_TITLE = "//*[contains(@id, 'analysis')]/td[text()='{WF_TITLE}']";
+
 
 }
--- test/functional/src/main/java/precisionFDA/pages/wf/WorkflowsPage.java
+++ test/functional/src/main/java/precisionFDA/pages/wf/WorkflowsPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 import precisionFDA.locators.WorkflowLocators;
+import precisionFDA.model.AppProfile;
+import precisionFDA.model.WorkflowProfile;
 import precisionFDA.pages.AbstractPage;
 import ru.yandex.qatools.htmlelements.element.Link;
 
@@ -29,6 +31,22 @@ public class WorkflowsPage extends AbstractPage {
         log.info("click Create Workflow");
         getCreateWorkflowButtonLink().click();
         return new CreateWorkflowPage(getDriver());
+    }
+
+    public CreatedWorkflowPage openCreatedWorkflow(WorkflowProfile workflowProfile) {
+        log.info("open created workflow");
+        waitUntilDisplayed(getLinkToCreatedWorkflowBy(workflowProfile), 1);
+        getDriver().findElement(getLinkToCreatedWorkflowBy(workflowProfile)).click();
+        return new CreatedWorkflowPage(getDriver());
+    }
+
+    public By getLinkToCreatedWorkflowBy(WorkflowProfile workflowProfile) {
+        String xpath = WorkflowLocators.LINK_TO_CREATED_WORKFLOW_TEMPLATE.replace("{WF_TITLE}", workflowProfile.getWfTitle());
+        return By.xpath(xpath);
+    }
+
+    public boolean isLinkToCreatedWorkflowDisplayed(WorkflowProfile workflowProfile) {
+        return isElementPresent(getLinkToCreatedWorkflowBy(workflowProfile), 10);
     }
 
 }
