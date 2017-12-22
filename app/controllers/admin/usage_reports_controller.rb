d76a6117b0c82e3210ee4248322e0bf1cd75d5a7
Viktor Polotebnov <vpolotebnov-c@dnanexus.com>
2017-12-22 20:45:18 +0300

    Updated usage CSV's filename.

--- app/controllers/admin/usage_reports_controller.rb
+++ app/controllers/admin/usage_reports_controller.rb
@@ -2,13 +2,15 @@ module Admin
   class UsageReportsController < BaseController
 
     def index
+      date_string = Time.now.strftime("%Y-%m-%d")
+
       @usage_grid = initialize_grid(UsageMetric, {
         order: "created_at",
         order_direction: "desc",
         per_page: 100,
         include: [:user],
         enable_export_to_csv: true,
-        csv_file_name: "usage",
+        csv_file_name: "users_usage_#{date_string}",
         name: "usage"
       })
 
