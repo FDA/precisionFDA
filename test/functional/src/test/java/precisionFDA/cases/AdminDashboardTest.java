2dcf1571312e58520b7ace8361d5d4c675789472
aabramenko <aabramenko-c@dnanexus.com>
2017-12-25 21:37:14 +0300

    dashboard

--- test/functional/src/test/java/precisionFDA/cases/AdminDashboardTest.java
+++ test/functional/src/test/java/precisionFDA/cases/AdminDashboardTest.java
@@ -2,17 +2,12 @@ package precisionFDA.cases;
 
 import org.testng.annotations.Test;
 import precisionFDA.data.TestUserData;
-import precisionFDA.model.AppProfile;
 import precisionFDA.model.UserProfile;
-import precisionFDA.pages.apps.*;
+import precisionFDA.pages.dashboard.ActivityReportsPage;
 import precisionFDA.pages.dashboard.AdminDashboardPage;
 import precisionFDA.pages.overview.OverviewPage;
 import ru.yandex.qatools.htmlelements.annotations.Name;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.testng.Assert.assertTrue;
-import static precisionFDA.data.TestAppData.*;
-import static precisionFDA.data.TestRunData.*;
 import static precisionFDA.utils.Utils.*;
 
 @Name("Admin dashboard test suite")
@@ -47,11 +42,12 @@ public class AdminDashboardTest extends AbstractTest {
 
         assertThat(
                 dashboardPage.isActiveUsersLinkDisplayed())
-                .as("Active Users is displayed")
+                .as("Active Users button is displayed")
                 .isTrue();
 
         removeActiveUsersFileFromDownloads();
         dashboardPage.clickActiveUsers();
+        alertAccept(5, 500);
         dashboardPage.waitUntilActiveUsersFileIsDownloaded();
 
         assertThat(
@@ -62,6 +58,51 @@ public class AdminDashboardTest extends AbstractTest {
         assertThat(
                 dashboardPage.isActiveUsersFileCorrect())
                 .as("Active Users file is correct one")
+                .isTrue();
+    }
+
+    @Test(dependsOnMethods = {"successfulLogin"})
+    public void verifyActivityReportingPage() {
+        printTestHeader("Test Case: verify all widgets are displayed");
+
+        AdminDashboardPage dashboardPage = openOverviewPage().openAdminDashboardPage();
+
+        assertThat(
+                dashboardPage.isActivityReportsLinkDisplayed())
+                .as("Activity Reports button is displayed")
+                .isTrue();
+
+        ActivityReportsPage reportsPage = dashboardPage.clickActivityReports();
+
+        assertThat(
+                reportsPage.areChartsDisplayed())
+                .as("All charts are displayed")
+                .isTrue();
+    }
+
+    @Test(dependsOnMethods = {"successfulLogin"})
+    public void handleWrongPeriodOfActivityReport() {
+        printTestHeader("Test Case: try to set wrong date period on the Activity Report page");
+
+        AdminDashboardPage dashboardPage = openOverviewPage().openAdminDashboardPage();
+        ActivityReportsPage reportsPage = dashboardPage.clickActivityReports();
+
+        assertThat(
+                reportsPage.isFromDateInputDisplayed())
+                .as("From Date input is displayed")
+                .isTrue();
+
+        assertThat(
+                reportsPage.isToDateInputDisplayed())
+                .as("To Date input is displayed")
+                .isTrue();
+
+        reportsPage.setFromDateAsFirstDayNextMonth();
+        reportsPage.clickSubmitDatePeriod();
+
+        assertThat(
+                reportsPage.isWrongPeriodErrorDisplayed())
+                .as("Wrong Period error is displayed")
                 .isTrue();
     }
 
