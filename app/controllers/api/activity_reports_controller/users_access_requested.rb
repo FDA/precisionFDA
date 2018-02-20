9df758e9a95505faf5051539d9466a30a909d580
Mikhail Maltcev <mmaltcev-c@dnanexus.com>
2018-02-20 11:33:02 +0300

    PFDA-395. Auto logged out after 15 minutes.

--- app/controllers/api/activity_reports_controller/users_access_requested.rb
+++ app/controllers/api/activity_reports_controller/users_access_requested.rb
@@ -1,6 +1,6 @@
-module Admin
+module Api
   class ActivityReportsController
-    class UsersSignedUpForChallenge < AbstractData
+    class UsersAccessRequested < AbstractData
 
       def total
         collection.count
@@ -27,7 +27,7 @@ module Admin
       private
 
       def collection
-        Event::SignedUpForChallenge.date_range(start_date, end_date)
+        Event::UserAccessRequested.date_range(start_date, end_date)
       end
 
     end
--- config/environments/ui_test.rb
+++ config/environments/ui_test.rb
@@ -39,6 +39,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.active_record.migration_error = :page_load
+
   # SSL
   config.force_ssl = true
 
