8cec58f5bde37e63471afcc360ce5a9a6b51afc1
Mikhail Maltcev <mmaltcev-c@dnanexus.com>
2018-03-22 20:29:26 +0300

    PFDA-419. Refactoring graphs

---
+++ app/decorators/graph_decorator/file_node.rb
@@ -0,0 +1,12 @@
+module GraphDecorator
+  class FileNode < BaseNode
+
+    def children
+      return [] unless record.accessible_by?(context)
+      return [build_child(record.parent)] if record.parent_type == "Job"
+      return [build_child(record.parent)] if record.parent_type == "Comparison"
+      []
+    end
+
+  end
+end
