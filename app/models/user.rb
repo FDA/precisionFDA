b648f9d5616865c05f53e276dd0622c171cfe94f
pamella tater <ptater@dnanexus.com>
2018-04-04 08:27:42 -0700

    Add martin.bednar as admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -144,7 +144,7 @@ class User < ActiveRecord::Base
     if Rails.env.production? && ENV["DNANEXUS_BACKEND"] == "production"
       dxuser == "elaine.johanson" || dxuser == "ruth.bandler"
     else
-      ((["precisionfda", "precisionfda_dev", "dnanexus"].include?(org.handle)) && org.admin_id == id) || ["alan.fdauser", "Adam.Berger@fda.hhs.gov", "mbednar", "Zivana.Tezak@fda.hhs.gov", "singeradmin.pfdadev", "pamella.tater.2", "ezekiel.maier", "vijay.kandali"].include?(dxuser)
+      ((["precisionfda", "precisionfda_dev", "dnanexus"].include?(org.handle)) && org.admin_id == id) || ["alan.fdauser", "Adam.Berger@fda.hhs.gov", "martin.bednar", "Zivana.Tezak@fda.hhs.gov", "singeradmin.pfdadev", "pamella.tater.2", "ezekiel.maier", "vijay.kandali"].include?(dxuser)
     end
   end
 
