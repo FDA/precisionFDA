e5d680afa55230057af05f90b9bd531081661f78
Mikhail Maltcev <mmaltcev-c@dnanexus.com>
2018-11-22 16:39:11 +0300

    PFDA-506. Task system. Update cron schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,27 +1,18 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+set :bundle_command, "/usr/local/bin/bundle exec"
 
-# Example:
-#
-# set :output, "cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+[
+  "DNANEXUS_BACKEND",
+  "SALESFORCE_USERNAME",
+  "SALESFORCE_PASSWORD",
+  "SALESFORCE_SECRET_TOKEN",
+  "SALESFORCE_HOST",
+  "SALESFORCE_FDA_EMAIL_ID",
+  "SALESFORCE_NOTIFIER_EMAIL_ID",
+].each do |name|
+  env name, ENV[name]
+end
 
-# Learn more: http://github.com/javan/whenever
-
-# set :environment, ENV['RAILS_ENV']
-
-# every 1.hour do
-#   runner "TasksChecker.check_tasks_for_failed_response_deadline"
-#   runner "TasksChecker.check_tasks_for_failed_completion_deadline"
-# end
+every 1.hour do
+  runner "TasksChecker.check_tasks_for_failed_response_deadline"
+  runner "TasksChecker.check_tasks_for_failed_completion_deadline"
+end
