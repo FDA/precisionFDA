11870be9b4309f914b7365d2a1bfd0508bdcbc07
Mikhail Maltcev <mmaltcev-c@dnanexus.com>
2018-11-26 14:18:58 +0300

    PFDA-510 Review Spaces. dxid to uid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181106134112) do
+ActiveRecord::Schema.define(version: 20181126083825) do
 
   create_table "accepted_licenses", force: :cascade do |t|
     t.integer  "license_id", limit: 4
