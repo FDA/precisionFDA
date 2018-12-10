530f9d6d6ebbce534093a116c49e2b19eca24cd0
Aleksandr Moroz <amoroz-cf@dnanexus.com>
2018-12-10 19:16:42 +0300

    PFDA-610: update WDL exporter readme and format

--- app/services/wdl_exporter/workflow_readme.rb
+++ app/services/wdl_exporter/workflow_readme.rb
@@ -4,14 +4,23 @@ module WdlExporter::WorkflowReadme
     <<-EOH
 ##{workflow.name}
 
-To execute this app locally, please ensure you have Docker (get.docker.com) and Cromwell (https://github.com/broadinstitute/cromwell) and run:
+To execute this app locally, please ensure you have Docker (get.docker.com), Cromwell (https://github.com/broadinstitute/cromwell) and WDLTool (https://github.com/broadinstitute/wdltool) and run:
 
 ```
-java -jar /path/to/cromwell.jar #{workflow.name}.wdl -i inputs.json
+docker build . -t #{workflow.name}
+java -jar /path/to/wdltool.jar inputs #{workflow.name}.wdl > inputs.json
+java -jar /path/to/cromwell.jar run #{workflow.name}.wdl -i inputs.json
 ```
 
 where inputs.json is a standard WDL input file definition (see https://software.broadinstitute.org/wdl/ for examples).
+
+The output directory structure would be the following:
+
+cromwell-workflow-logs/
+cromwell-executions/#{workflow.name}/<hash>/call-<app1>/execution/
+cromwell-executions/#{workflow.name}/<hash>/call-<app2>/execution/
+....
+cromwell-executions/#{workflow.name}/<hash>/call-<appN>/execution/
     EOH
   end
-
 end
