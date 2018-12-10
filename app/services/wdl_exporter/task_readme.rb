6df25a96895ed932612743d38246923b46c7fb27
Aleksandr Moroz <amoroz-cf@dnanexus.com>
2018-12-10 19:16:42 +0300

    PFDA-610: update WDL exporter readme and format

--- app/services/wdl_exporter/task_readme.rb
+++ app/services/wdl_exporter/task_readme.rb
@@ -4,14 +4,23 @@ module WdlExporter::TaskReadme
     <<-EOH
 ##{app.name}
 
-To execute this app locally, please ensure you have Docker (get.docker.com) and Cromwell (https://github.com/broadinstitute/cromwell) and run:
+To execute this app locally, please ensure you have Docker (get.docker.com), Cromwell (https://github.com/broadinstitute/cromwell) and WDLTool (https://github.com/broadinstitute/wdltool) and run:
 
 ```
-java -jar /path/to/cromwell.jar #{app.name}.wdl -i inputs.json
+docker build . -t #{app.name}
+java -jar /path/to/wdltool.jar inputs #{app.name}.wdl > inputs.json
+java -jar /path/to/cromwell.jar run #{app.name}.wdl -i inputs.json
 ```
 
 where inputs.json is a standard WDL input file definition (see https://software.broadinstitute.org/wdl/ for examples).
+
+The output directory structure would be the following:
+
+cromwell-workflow-logs/
+cromwell-executions/single_task/<hash>/call-#{app.name}/execution/
+cromwell-executions/single_task/<hash>/call-#{app.name}/execution/fetched_file/
+cromwell-executions/single_task/<hash>/call-#{app.name}/execution/glob-<hash>/
+cromwell-executions/single_task/<hash>/call-#{app.name}/execution/tmp/
     EOH
   end
-
 end
