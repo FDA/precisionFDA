6595ba8566ce25303e7ed334e56a2d84a62d1d2d
Mikhail Maltcev <mmaltcev-c@dnanexus.com>
2018-12-17 15:26:58 +0300

    Fix visibility of the spaces' button for site admins

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,9 +2,11 @@ class UserPolicy
 
   class << self
 
+    # Visibility of the spaces' button
     def can_see_spaces?(context)
       return false unless context.logged_in?
       return true if context.user.review_space_admin?
+      return true if context.user.can_administer_site?
 
       context.user.space_memberships.active.count > 0
     end
