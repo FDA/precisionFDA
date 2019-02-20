3c363eec363411226516967fe421d910553f461f
Aleksei Ivanishchev <BlookHo@users.noreply.github.com>
2019-02-20 16:32:26 +0300

    PFDA-733_analyses_not_displayed_on_workflows_page_shared_in_space) (#400)
    - series scope updated in WorkflowPublisher
     - run_workflows query added in index
     - @run_workflows added in index.html

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -106,9 +106,15 @@ class WorkflowsController < ApplicationController
       order: 'analyses.id',
       order_direction: 'desc',
       per_page: 100,)
+    
     @my_workflows = WorkflowSeries.includes(latest_revision_workflow: [user: :org])
       .editable_by(@context).order(name: :asc)
       .map { |series| series.latest_accessible(@context) }.compact
+
+    @run_workflows = WorkflowSeries.eager_load(latest_revision_workflow: [user: :org])
+                      .accessible_by(@context).order(name: :asc)
+                       .where.not(user_id: @context.user_id)
+                       .map { |series| series.latest_accessible(@context) }.compact
 
     js js_param.merge(batches: batch_hash)
   end
