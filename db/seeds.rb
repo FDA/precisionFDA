2a04f6a8e67252aa9d438c62e0c9fd28b3fabb22
Ekaterina Barasheva <3274811+latefebruary@users.noreply.github.com>
2019-03-06 16:51:53 +0300

    Fix seeds (#420)

--- db/seeds.rb
+++ db/seeds.rb
@@ -78,11 +78,11 @@ ActiveRecord::Base.transaction do
     end_at: 2.weeks.from_now
   )
 
-  User.create!(
+  User.new(
     dxuser: CHALLENGE_BOT_DX_USER,
     private_files_project: CHALLENGE_BOT_PRIVATE_FILES_PROJECT,
     public_files_project: CHALLENGE_BOT_PUBLIC_FILES_PROJECT
-  )
+  ).save!(validate: false)
 
   # load articles generated from stuff.
   SeedNewsItems.new.up
