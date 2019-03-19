065b70534974278693925c6b1f83d895fffed91b
Zverev Dmitrii <45661958+Dmitrii-Zverev@users.noreply.github.com>
2019-03-19 16:15:36 +0300

    Return if an email wasn't sent (#439)

--- app/services/profiles/email_synchronizer.rb
+++ app/services/profiles/email_synchronizer.rb
@@ -5,6 +5,7 @@ module Profiles
     def self.call(user, context)
       api = DNAnexusAuth.new(DNANEXUS_AUTHSERVER_URI, context.token)
       response = api.call("system", AUTH_METHOD)
+      return if response['email'].blank?
       if response['email'].downcase == user.profile.email.downcase
         User.transaction do
           user.update!(email: response['email'], normalized_email: response['email'].downcase)
