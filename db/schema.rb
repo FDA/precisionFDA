4862a3f8200fa6925edbe253c135c96241eacb53
Ekaterina Barasheva <3274811+latefebruary@users.noreply.github.com>
2019-04-05 15:51:29 +0300

    PFDA-662 Make notifications configurable by user role (#457)
    * Notifications [WIP]
    
    * Fix schema
    
    * Remove notifications prefix
    
    * Use new notification preferences for space event service
    
    * Send notifications if user has more than one role for a space
    
    * Add file_deleted and asset_deleted activity types to space notifications sender
    
    * save settings for all roles at same time
    
    * Hide app_or_workflow_status_changed preference
    
    * Add new email for a case when content was deleted from space
    
    * Add task accepted space event and enable task accepted notifications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190322143409) do
+ActiveRecord::Schema.define(version: 20190325144741) do
 
   create_table "accepted_licenses", force: :cascade do |t|
     t.integer  "license_id", limit: 4
