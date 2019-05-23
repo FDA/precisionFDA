e33d4fd295200beee107f33ec0039ff237d0f851
Aleksei Ivanishchev <aivanishchev-cf@dnanexus.com>
2019-05-23 22:06:54 +0300

    PFDA-823_File_attachments_have_disappeared_from_old_discussions
     - create a fixing reversible migration in attachments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190325144741) do
+ActiveRecord::Schema.define(version: 20190523185311) do
 
   create_table "accepted_licenses", force: :cascade do |t|
     t.integer  "license_id", limit: 4
