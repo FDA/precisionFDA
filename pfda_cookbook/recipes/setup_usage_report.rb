aca63fe73f2ae2cd309ac41a2a7c03e3bf02dddc
Aleksandr Moroz <amoroz-cf@dnanexus.com>
2019-05-29 19:56:55 +0300

    Fix usage_report daily cron job

--- pfda_cookbook/recipes/setup_usage_report.rb
+++ pfda_cookbook/recipes/setup_usage_report.rb
@@ -1,9 +1,9 @@
 file '/etc/cron.daily/usage_report' do
   content <<-EOF
-  #!/bin/bash
-  su -c 'cd /srv/www/precision_fda/current && bundle exec rake usage_report:generate' -s /bin/bash deploy
+#!/bin/bash
 
-  EOF
+su -c 'cd /srv/www/precision_fda/current && RAILS_ENV=production bundle exec rake usage_report:generate' -s /bin/bash deploy
+EOF
 
   mode '755'
 end
