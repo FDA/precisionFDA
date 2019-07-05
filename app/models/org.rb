8bd729677127a00deb8fbec28a3333a70085d8fe
Aleksei Ivanishchev <aivanishchev-cf@dnanexus.com>
2019-07-05 20:09:40 +0300

    PFDA-929_Admin_Dashboard_error_in_generate_report
     - add Org.reports
     - fix bug when org and user are inconsistent
     - add spec to Org model
     - exclude Org.report method from controller
     - Org specs created

--- app/models/org.rb
+++ app/models/org.rb
@@ -38,7 +38,53 @@ class Org < ActiveRecord::Base
   end
 
   def self.real_orgs
-    return where(singular: false)
+    where(singular: false)
+  end
+
+  def self.reports(sheet)
+    order(:name).find_each do |org|
+      %w(name handle address phone).each do |label|
+        sheet.add_row ["Organization #{label}:", org.send(label)]
+      end
+      org.admin_in_report(sheet)
+      org.users_in_report(sheet)
+    end
+    sheet
+  end
+
+  def admin_in_report(sheet)
+    if admin.blank?
+      sheet.add_row ["Warning: No admin exists for this Org: #{handle}"]
+    else
+      sheet.add_row [
+        "Admin:", "", admin.dxuser, admin.first_name,
+        admin.last_name, admin.email, admin.created_at.strftime("%Y-%m-%d %H:%M"),
+        admin.last_login ? admin.last_login.strftime("%Y-%m-%d %H:%M") : "",
+        admin.user_files.sum(:file_size), admin.app_series.count, admin.jobs.count,
+      ]
+    end
+  end
+
+  def users_in_report(sheet)
+    if users.exists?
+      users = self.users.order(:dxuser).all.reject { |u| u.id == admin_id }
+      users.each do |user|
+        if user.blank?
+          sheet.add_row ["No data for user #{user.dxuser} in Org: #{handle}"]
+        else
+          role = user.id == admin_id ? "Admin:" : "Member:"
+          sheet.add_row [
+            role, "", user.dxuser, user.first_name, user.last_name, user.email,
+            user.created_at.strftime("%Y-%m-%d %H:%M"),
+            user.last_login ? user.last_login.strftime("%Y-%m-%d %H:%M") : "",
+            user.user_files.sum(:file_size), user.app_series.count, user.jobs.count,
+          ]
+        end
+      end
+    else
+      sheet.add_row ["Warning: No users (Admin or Member) exists for this Org: #{handle}"]
+    end
+    sheet.add_row
   end
 
   def real_org?
