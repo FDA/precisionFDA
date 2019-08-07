bea0c0d9e723f731eb5f5cfd408fdc923b01173a
Aleksandr Moroz <amoroz-cf@dnanexus.com>
2019-08-07 14:59:47 +0300

    PFDA-974, PFDA-998: job with int/boolean input failed with error 'the value is not int/boolean'

--- spec/models/app/cwl_parser_spec.rb
+++ spec/models/app/cwl_parser_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe App::CwlParser do
-  let!(:cwl) { CwlPresenter.new(cwl_file_content) }
   let(:cwl_file_content) { IO.read(Rails.root.join('spec/support/files/docker_pull.cwl')) }
+  let!(:cwl) { CwlPresenter.new(cwl_file_content) }
 
-  describe '#parse' do
+  describe '.parse' do
     subject { described_class.parse(cwl) }
 
     it 'returns a proper code with input settings' do
-      expect(subject[:code]).to include <<-SUBCODE
-cat <<EOF > input_settings.json
-{"in_file":{"class":"File","path":"${in_file_path}"},"post_address":"${post_address}","test_type_simplification":"${test_type_simplification}"}
-EOF
-      SUBCODE
+      expect(subject[:code]).to include \
+        %(cat <<EOF > input_settings.json\n) +
+        %({"in_file":{"class":"File","path":"${in_file_path}"},"post_address":"${post_address}",) +
+        %("optional_string":"${optional_string}","bool_flag": ${bool_flag},) +
+        %("int_input": ${int_input},"float_input": ${float_input},) +
+        %("double_input": ${double_input}}\nEOF)
     end
 
     it 'returns a proper code with a cwltool command' do
-      expect(subject[:code]).to include <<-SUBCODE
-cwltool --user-space-docker-cmd=dx-docker description.cwl input_settings.json > cwl_job_outputs.json
-      SUBCODE
+      expect(subject[:code]).to include \
+        "cwltool --user-space-docker-cmd=dx-docker description.cwl " +
+        "input_settings.json > cwl_job_outputs.json"
     end
 
     it 'returns a proper code with a python script' do
