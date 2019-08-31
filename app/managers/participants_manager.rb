9f6f3528aecc6fb73c549885cd06a2f93a1e1078
Aleksei Ivanishchev <BlookHo@users.noreply.github.com>
2019-08-31 14:33:57 +0300

    PFDA-1017_Admin_dashboard_update_participant_error_500
     - use uid for participants file managing

--- app/managers/participants_manager.rb
+++ app/managers/participants_manager.rb
@@ -1,7 +1,8 @@
 module ParticipantsManager
 
   def self.save(context, participant, params)
-    file = UserFile.accessible_by(context).find_by_uid(params[:node_dxid])
+    uid = Node.find_by(dxid: params[:node_dxid]).uid
+    file = UserFile.accessible_by(context).find_by_uid(uid)
     file_id = file.id if file && file.state == "closed" && file.file_size <= 5000000
 
     participant.node_id = file_id
