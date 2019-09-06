6918d523de8e97a0a84151a9b4cebf4dc703c748
Aleksandr Moroz <44093081+amorozdna@users.noreply.github.com>
2019-09-06 13:26:19 +0300

    Fix (+ refactor) failed apps_controller spec (#654)

--- spec/controllers/api/apps_controller_spec.rb
+++ spec/controllers/api/apps_controller_spec.rb
@@ -1,60 +1,60 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Api::AppsController, type: :controller do
   let(:user) { create(:user, dxuser: "user") }
-  let(:asset) { create(:asset, dxid: 'file-test', user_id: user.id) }
+  let(:asset) { create(:asset, dxid: "file-test", user_id: user.id) }
 
   let(:input) do
     [{ name: "anything", class: "string", optional: false, label: "anything", help: "anything" }]
   end
 
   let(:output) do
-    [{ name:  "my_file", class:  "file", optional:  false, label:  "my_file", help:  "my_file" }]
+    [{ name: "my_file", class:  "file", optional: false, label: "my_file", help: "my_file" }]
   end
 
   describe "POST create" do
-    before { authenticate!(user) }
+    before do
+      authenticate!(user)
+      post :create,
+           name:   "test-name",
+           title:  "test-title",
+           readme: "test-readme",
+           internet_access: false,
+           instance_type: "baseline-8",
+           ordered_assets: [asset.uid],
+           packages: %w(ikiwiki),
+           code: "test-code",
+           is_new: true,
+           input_spec: input,
+           output_spec: output
+    end
 
-    it "creates an app" do
-      expect(Event::AppCreated).to receive(:create_for)
+    it "creates an event" do
+      expect(Event::AppCreated).to have_received(:create_for)
+    end
 
-      post :create, {
-        name: 'test-name',
-        title: 'test-title',
-        readme: 'test-readme',
-        internet_access: false,
-        instance_type: 'baseline-8',
-        ordered_assets: [asset.uid],
-        packages: ['ikiwiki'],
-        code: 'test-code',
-        is_new: true,
-        input_spec: input,
-        output_spec: output,
-      }
-
+    it "creates an applet" do
       expect(WebMock).to have_requested(:post, "#{DNANEXUS_APISERVER_URI}applet/new").with(body: {
         project: "project-test",
-        inputSpec: [
-          { name: "anything", class: "string", optional: false, label: "anything", help: "anything" }
-        ],
-        outputSpec: [
-          { name: "my_file", class: "file", optional: false, label: "my_file", help: "my_file" }
-        ],
+        inputSpec: [{ name: "anything", class: "string", optional: false,
+                      label: "anything", help: "anything" }],
+        outputSpec: [{ name: "my_file", class: "file", optional: false,
+                       label: "my_file", help: "my_file" }],
         runSpec: {
           code: "dx cat project-Bk0YZkj0YkbBg6bk38PzQkVV:/appkit.tgz | " \
                  "tar -z -x -C / --no-same-owner --no-same-permissions -f " \
                  "-\nsource /usr/lib/app-prologue\ntest-code\n{ set +x; } " \
                  "2\u003e/dev/null\nsource /usr/lib/app-epilogue\n",
           interpreter: "bash",
-          systemRequirements: { "*": { instanceType: "mem1_ssd1_x8_fedramp" }},
-          distribution: "Ubuntu",
-          release: "14.04",
-          execDepends: [{ name: "ikiwiki" }]
+          systemRequirements: { "*": { instanceType: "mem1_ssd1_x8_fedramp" } },
+          distribution: "Ubuntu", release: "14.04", execDepends: [{ name: "ikiwiki" }]
         },
         dxapi: "1.0.0",
-        access: {}
+        access: {},
       })
+    end
 
+    it "creates an app" do
       expect(WebMock).to have_requested(:post, "#{DNANEXUS_APISERVER_URI}app/new").with(body: {
         applet: nil,
         name: "-user-test-name",
@@ -65,26 +65,29 @@ RSpec.describe Api::AppsController, type: :controller do
         resources: ["file-test"],
         details: { ordered_assets: ["file-test"] },
         openSource: false,
-        billTo: "org-pfda..org1",
+        billTo: user.billto,
         access: {},
       })
 
+      expect(response).to have_http_status(200)
+
+      expect(last_app.attributes.slice(%w(instance_type internet_access code))).to include(
+        "instance_type" => "baseline-8",
+        "internet_access" => false,
+        "code" => "test-code",
+        "scope" => "private",
+      )
+    end
+
+    it "removes an applet" do
       expect(WebMock).to(
         have_requested(
           :post,
-          "#{DNANEXUS_APISERVER_URI}project-test/removeObjects"
+          "#{DNANEXUS_APISERVER_URI}project-test/removeObjects",
         ).with(
-          body: { objects: [nil] }
-        )
+          body: { objects: [nil] },
+        ),
       )
-
-      expect(App.where(title: 'test-title', readme: 'test-readme', scope: 'private',).first)
-        .to be_present
-
-      expect(last_app.instance_type).to eq('baseline-8')
-      expect(last_app.internet_access).to eq(false)
-      expect(last_app.code).to eq('test-code')
-      expect(response).to have_http_status(200)
     end
   end
 end
