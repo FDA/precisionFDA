d9665362cf5066266f161cbb1189e80e43614f94
Viktor Polotebnov <vpolotebnov-c@dnanexus.com>
2019-09-18 18:17:38 +0300

    PFDA-1060: Opening space log crashes app (#645)
    * Rename factory job to jobs in order factory to be visible for annotate gem.
    
    * Remove empty Job model test from test/models.
    
    * Update code style in jobs_controller.
    
    * Remove unused jobs fixture.
    
    * Moved Job.publish to service.
    
    * Annotate App and Space.
    
    * Remove unused fixtures and minitests for App and Space.
    
    * Add method to DNAnexusAPI for clonning projects.
    
    * Add some docs for JobPublisher.
    
    * Add NotASpaceError class.
    
    * Change JobPublisher implementation from class to instance.
    
    * Fix comment indentation.
    
    * Remove duplicated factory.

--- app/models/app.rb
+++ app/models/app.rb
@@ -3,18 +3,21 @@
 # Table name: apps
 #
 #  id            :integer          not null, primary key
-#  dxid          :string
-#  version       :string
+#  dxid          :string(255)
+#  version       :string(255)
 #  revision      :integer
-#  title         :string
-#  readme        :text
+#  title         :string(255)
+#  readme        :text(65535)
 #  user_id       :integer
-#  scope         :string
-#  spec          :text
-#  internal      :text
+#  scope         :string(255)
+#  spec          :text(65535)
+#  internal      :text(65535)
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #  app_series_id :integer
+#  verified      :boolean          default(FALSE), not null
+#  uid           :string(255)
+#  dev_group     :string(255)
 #
 
 class App < ApplicationRecord
---
+++ spec/factories/jobs.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: jobs
+#
+#  id              :integer          not null, primary key
+#  dxid            :string(255)
+#  app_id          :integer
+#  project         :string(255)
+#  run_data        :text(65535)
+#  describe        :text(65535)
+#  provenance      :text(65535)
+#  state           :string(255)
+#  name            :string(255)
+#  user_id         :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  app_series_id   :integer
+#  scope           :string(255)
+#  analysis_id     :integer
+#  uid             :string(255)
+#  local_folder_id :integer
+#
+
+FactoryBot.define do
+  factory :job do
+    name { "default_title" }
+    association :app_series
+    sequence(:dxid) { "job-#{SecureRandom.hex(12)}" }
+  end
+end
