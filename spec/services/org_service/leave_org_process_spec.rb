3d75ccc3989750e308910de46ba1b9faab98cc3d
Viktor Polotebnov <vpolotebnov-c@dnanexus.com>
2019-11-07 12:56:57 +0300

    PFDA-1179: Fix for site admin organization leaving (#719)
    * Increase number of nested examples in Rubocop's config.
    
    * Allow site admin to leave organization.
    
    * Generate html-safe lastnames for users.
    
    * Make upcased only first symbols of firstname and lastname.

--- spec/services/org_service/leave_org_process_spec.rb
+++ spec/services/org_service/leave_org_process_spec.rb
@@ -138,6 +138,29 @@ RSpec.describe OrgService::LeaveOrgProcess do
           )
         end
 
+        context "when org_invite raises self-invite error" do
+          before do
+            allow(admin_api).to receive(:org_invite).
+              and_raise(Net::HTTPServerException.
+                new("Cannot invite yourself to an organization", nil))
+          end
+
+          it "doesn't raise error" do
+            expect { service.call(org, admin) }.not_to raise_error
+          end
+        end
+
+        context "when org_invite raises not self-invite error" do
+          before do
+            allow(admin_api).to receive(:org_invite).
+              and_raise(Net::HTTPServerException.new("Some error", nil))
+          end
+
+          it "re-raises error" do
+            expect { service.call(org, admin) }.to raise_error(Net::HTTPServerException)
+          end
+        end
+
         it "renames new private projects" do
           service.call(org, admin)
 
