d273ccad33d608079467b4a5cf708308b0e89030
Viktor Polotebnov <vpolotebnov-c@dnanexus.com>
2019-11-13 17:56:18 +0300

    PFDA-815: Add usage reports generation to whenever's schedule. (#726)
    * Add usage reports generation to whenever's schedule.
    
    * Add cron log.
    
    * Add SECRET_KEY_BASE to schedule.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,14 +1,16 @@
 set :bundle_command, "/usr/local/bin/bundle exec"
+set :output, "log/cron.log"
 
-[
-  "DNANEXUS_BACKEND",
-  "SALESFORCE_USERNAME",
-  "SALESFORCE_PASSWORD",
-  "SALESFORCE_SECRET_TOKEN",
-  "SALESFORCE_HOST",
-  "SALESFORCE_FDA_EMAIL_ID",
-  "SALESFORCE_NOTIFIER_EMAIL_ID",
-].each do |name|
+%w(
+  DNANEXUS_BACKEND
+  SALESFORCE_USERNAME
+  SALESFORCE_PASSWORD
+  SALESFORCE_SECRET_TOKEN
+  SALESFORCE_HOST
+  SALESFORCE_FDA_EMAIL_ID
+  SALESFORCE_NOTIFIER_EMAIL_ID
+  SECRET_KEY_BASE
+).each do |name|
   env name, ENV[name]
 end
 
@@ -23,4 +25,8 @@ end
 
 every 1.week do
   runner "PhoneConfirmationService.check_expired_phone_confirmations"
+end
+
+every 1.day do
+  rake "usage_report:generate"
 end
