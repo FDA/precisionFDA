97602640baab36152714335e98af1f2f364b387c
Aleksandr Moroz <amoroz-cf@dnanexus.com>
2019-11-20 20:02:24 +0300

    Fix warnings in app/decorators

--- app/decorators/graph_decorator/file_node.rb
+++ app/decorators/graph_decorator/file_node.rb
@@ -1,13 +1,12 @@
 module GraphDecorator
   class FileNode < BaseNode
-
     def children
       return [] unless record.accessible_by?(context)
       return [build_child(record.parent)] if record.parent_type == "Node"
       return [build_child(record.parent)] if record.parent_type == "Job"
       return [build_child(record.parent)] if record.parent_type == "Comparison"
+
       []
     end
-
   end
 end
