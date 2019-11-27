75afdd15c4b74022e7a36facf8071e181ad4a837
Aleksandr Moroz <amoroz-cf@dnanexus.com>
2019-11-27 11:16:44 +0300

    Add average_scheduled_poll_interval to sidekiq's config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,6 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://#{ENV['REDIS_URL']}/0", network_timeout: 5 }
+  config.average_scheduled_poll_interval = 5 # default is 5
 end
 
 Sidekiq.configure_client do |config|
