a38900bcf27f5921bb97a87072e98d099783077a
Viktor Polotebnov <vpolotebnov-c@dnanexus.com>
2019-12-12 16:14:43 +0300

    Add invitationSearcher.

---
+++ app/services/invitation_searcher.rb
@@ -0,0 +1,44 @@
+# Responsible for users invitation searching.
+class InvitationSearcher
+  class << self
+    # Performs user searching by invitation's email, first name or last name.
+    # @param query [String] Query to search users.
+    # @return [ActiveRecord::Relation<Invitation>] Found invitations.
+    def call(query)
+      query.present? ? Invitation.where(conditions(query)) : Invitation.none
+    end
+
+    private
+
+    # Sanitizes query and adds wildcards for use in SQL LIKE.
+    # @param query [String] Query to sanitize.
+    # @return [String] Sanitized query.
+    def sanitized_like_query(query)
+      "%#{ActiveRecord::Base.sanitize_sql_like(query)}%"
+    end
+
+    # Returns search conditions.
+    # @param query [String] Search query.
+    # @return [Arel::Nodes::Node] Arel node containing search conditions.
+    def conditions(query)
+      or_conditions = fields.inject(nil) do |relation, field|
+        condition = invitations[field].matches(sanitized_like_query(query))
+        relation ? relation.or(condition) : condition
+      end
+
+      or_conditions.and(invitations[:user_id].eq(nil))
+    end
+
+    # Returns Arel table for Invitation model.
+    # @return [Arel::Table] Arel table.
+    def invitations
+      Invitation.arel_table
+    end
+
+    # Returns set of fields to search invitation by.
+    # @return [Array<String>] Search fields.
+    def fields
+      %w(email first_name last_name).freeze
+    end
+  end
+end
