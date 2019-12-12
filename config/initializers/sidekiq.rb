e70e2de1f59753c5e5464d6827a4a2072ac48397
Aleksandr Moroz <amoroz-cf@dnanexus.com>
2019-12-12 20:23:38 +0300

    Add Redis AUTH password to Sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,11 @@
+redis_options = { url: ENV["REDIS_WORKER_URL"], network_timeout: 5 }
+redis_options.merge!(password: ENV["REDIS_AUTH"]) if Rails.env.production?
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDIS_WORKER_URL"], network_timeout: 5 }
-  config.average_scheduled_poll_interval = 5 # default is 5
+  config.redis = redis_options
+  config.average_scheduled_poll_interval = 5
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDIS_WORKER_URL"], network_timeout: 5 }
+  config.redis = redis_options
 end
