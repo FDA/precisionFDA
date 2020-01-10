900e33c7333df2a259783a95e3850f7e7700ac95
Viktor Polotebnov <vpolotebnov-c@dnanexus.com>
2020-01-10 17:16:08 +0300

    Add InvitationSerializer.

---
+++ app/serializers/invitation_serializer.rb
@@ -0,0 +1,25 @@
+# Serializes invitation
+class InvitationSerializer
+  # Serializes invitation.
+  # @param invitation [Invitation] Invitation to serialize.
+  # @return [Hash] Serialized invitation.
+  def self.call(invitation)
+    {
+      id: invitation.id,
+      first_name: invitation.first_name,
+      last_name: invitation.last_name,
+      email: invitation.email,
+      address1: invitation.address1,
+      address2: invitation.address2,
+      country_name: invitation.country.name,
+      country_id: invitation.country.id,
+      city: invitation.city,
+      us_state: invitation.us_state,
+      postal_code: invitation.postal_code,
+      phone: invitation.full_phone,
+      duns: invitation.duns,
+      organize_intent: invitation.organize_intent,
+      participate_intent: invitation.participate_intent,
+    }
+  end
+end
