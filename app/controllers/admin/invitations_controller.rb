212fc3277381f84157ba8f72b0fcd2671df1401f
Viktor Polotebnov <vpolotebnov-c@dnanexus.com>
2020-01-10 17:16:08 +0300

    Add InvitationSerializer.

--- app/controllers/admin/invitations_controller.rb
+++ app/controllers/admin/invitations_controller.rb
@@ -53,27 +53,10 @@ module Admin
     private
 
     def render_invitations(query, exclude)
-      invitations = InvitationSearcher.call(query, exclude).map do |invitation|
-        {
-          id: invitation.id,
-          first_name: invitation.first_name,
-          last_name: invitation.last_name,
-          email: invitation.email,
-          address1: invitation.address1,
-          address2: invitation.address2,
-          country_name: invitation.country.name,
-          country_id: invitation.country.id,
-          city: invitation.city,
-          us_state: invitation.us_state,
-          postal_code: invitation.postal_code,
-          phone: invitation.full_phone,
-          duns: invitation.duns,
-          organize_intent: invitation.organize_intent,
-          participate_intent: invitation.participate_intent,
-        }
-      end
+      invitations = InvitationSearcher.call(query, exclude)
+      serialized = invitations.map { |invitation| InvitationSerializer.call(invitation) }
 
-      render json: invitations
+      render json: serialized
     end
 
     def redirect_to_list
