355366d3b5e743a5d9dee85526a27b5cd8d1914b
Viktor Polotebnov <vpolotebnov-c@dnanexus.com>
2020-01-14 16:03:26 +0300

    Add yardoc for Naming.

--- app/controllers/concerns/naming.rb
+++ app/controllers/concerns/naming.rb
@@ -1,7 +1,11 @@
+# Provides username-related methods.
 module Naming
   extend ActiveSupport::Concern
 
   included do
+    # Tries to find unused username.
+    # @param username [String] Base name.
+    # @return [String] Unused username.
     def find_unused_username(username)
       api = DIContainer.resolve("api.user")
       UnusedUsernameGenerator.new(api).call(username)
