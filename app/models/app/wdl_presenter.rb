14bea9d8dd5c195cd9d72c6a5d09eda8a10a8b33
Aleksandr Moroz <44093081+amorozdna@users.noreply.github.com>
2020-01-22 16:22:55 +0300

    Remove release from App's WDLPresenter

--- app/models/app/wdl_presenter.rb
+++ app/models/app/wdl_presenter.rb
@@ -11,7 +11,7 @@ class App
     validates :tasks, length: { is: 1, message: "number is wrong" }
 
     attr_reader :raw
-    attr_accessor :asset, :release
+    attr_accessor :asset
 
     def initialize(wdl_text)
       @raw = wdl_text
@@ -29,7 +29,6 @@ class App
         code: code,
         packages: %w(openjdk-8-jre-headless),
         ordered_assets: Array(asset.try(:uid)),
-        release: release,
       }
     end
 
