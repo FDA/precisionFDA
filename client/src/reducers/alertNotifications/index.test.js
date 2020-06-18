174508d919e1203535bcf8e6bea1f9ab9d4e78f2
Aleksandr Moroz <amoroz-cf@dnanexus.com>
2020-06-18 18:44:59 +0300

    Show maximum 3 stacked alert notifications if a user does actions too fast

--- client/src/reducers/alertNotifications/index.test.js
+++ client/src/reducers/alertNotifications/index.test.js
@@ -5,6 +5,7 @@ import {
   showAlertAboveAll,
   hideAlert,
 } from '../../actions/alertNotifications'
+import { ALERT_MESSAGES_MAX_COUNT } from '../../constants'
 
 
 describe('alertNotifications reducer', () => {
@@ -12,6 +13,18 @@ describe('alertNotifications reducer', () => {
     const message = 'some message'
     const store = mockStore(reducer({}, showAlertAboveAll({ message })))
     expect(alertMessagesSelector(store.getState())[0].message).toBe(message)
+  })
+
+  it('Keeps only certain number of messages on the screen', () => {
+    const message = 'some message'
+    const initialState = {
+      alertNotifications: {
+        messages: [{ id: 1, message }, { id: 2, message }, { id: 3, message }],
+      },
+    }
+
+    const store = mockStore(reducer(initialState, showAlertAboveAll({ message })))
+    expect(alertMessagesSelector(store.getState())).toHaveLength(ALERT_MESSAGES_MAX_COUNT)
   })
 
   it('ALERT_HIDE_MESSAGE', () => {
