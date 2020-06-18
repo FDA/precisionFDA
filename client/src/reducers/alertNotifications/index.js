c0b9a05de3b415640d527b8a0668eac06fd45076
Aleksandr Moroz <amoroz-cf@dnanexus.com>
2020-06-18 18:44:59 +0300

    Show maximum 3 stacked alert notifications if a user does actions too fast

--- client/src/reducers/alertNotifications/index.js
+++ client/src/reducers/alertNotifications/index.js
@@ -4,6 +4,7 @@ import {
   ALERT_SHOW_ABOVE_ALL,
   ALERT_HIDE_MESSAGE,
 } from '../../actions/alertNotifications/types'
+import { ALERT_MESSAGES_MAX_COUNT } from '../../constants'
 import { mapToAlertNotification } from '../../views/shapes/AlertShape'
 
 
@@ -11,10 +12,11 @@ export default createReducer(initialState, {
   [ALERT_SHOW_ABOVE_ALL]: (state, alert) => ({
     ...state,
     messages: [
-      ...state.messages,
+      ...state.messages.slice(1 - ALERT_MESSAGES_MAX_COUNT),
       mapToAlertNotification(alert),
     ],
   }),
+
   [ALERT_HIDE_MESSAGE]: (state, id) => ({
     ...state,
     messages: [...state.messages.filter((alert) => alert.id !== id)],
