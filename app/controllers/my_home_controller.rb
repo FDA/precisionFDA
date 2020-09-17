55f7233ac58c3c7c3ef20ecc5e1f375a32050def
Aleksei Ivanishchev <aivanishchev-cf@dnanexus.com>
2020-09-17 14:25:23 +0300

    - add flash message to inform

--- app/controllers/my_home_controller.rb
+++ app/controllers/my_home_controller.rb
@@ -1,4 +1,5 @@
 class MyHomeController < ApplicationController
   def index
+    flash.now[:success] = "This view can be accessed temporarily in Development or UI-test environments ONLY!"
   end
 end
