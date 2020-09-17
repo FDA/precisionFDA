f259a6e91d3e80631feb2ef598c216fb9fc6b3a2
Aleksei Ivanishchev <aivanishchev-cf@dnanexus.com>
2020-09-17 14:30:44 +0300

    - add rdoc

--- app/controllers/my_home_controller.rb
+++ app/controllers/my_home_controller.rb
@@ -1,4 +1,5 @@
 class MyHomeController < ApplicationController
+  # temporal action for My Home render
   def index
     flash.now[:success] =
       "This view can be accessed temporarily in Development or UI-test environments ONLY!"
