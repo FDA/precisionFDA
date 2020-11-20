9aff32c3495527d97c545b93087863817f96f3b6
Kaja Behalova <kbehalova-cf@dnanexus.com>
2020-11-20 14:54:39 +0100

    add: sync-job tests and fixes

---
+++ https-apps-api/packages/worker/test/utils/create.ts
@@ -0,0 +1,56 @@
+import { EntityManager, wrap } from '@mikro-orm/core'
+import { entities } from '@pfda/https-apps-shared'
+import * as generate from './generate'
+// todo: should be in shared helpers
+
+const userHelper = {
+  create: (em: EntityManager, data?: Partial<typeof entities.User>) => {
+    const defaults = generate.user.simple()
+    const input = {
+      ...defaults,
+      ...data,
+    }
+    const user = wrap(new entities.User()).assign(input, { em })
+    em.persist(user)
+    return user
+  },
+}
+
+const jobHelper = {
+  create: (
+    em: EntityManager,
+    references: {
+      user: InstanceType<typeof entities.User>
+      app?: InstanceType<typeof entities.App>
+    },
+    data?: Partial<InstanceType<typeof entities.Job>>,
+  ) => {
+    const defaults = generate.job.simple()
+    const input = {
+      ...defaults,
+      ...data,
+    }
+    const job = wrap(new entities.Job(references.user, references.app)).assign(input, { em })
+    em.persist(job)
+    return job
+  },
+}
+
+const appHelper = {
+  create: (
+    em: EntityManager,
+    references: { user: InstanceType<typeof entities.User> },
+    data?: Partial<InstanceType<typeof entities.App>>,
+  ) => {
+    const defaults = generate.app.simple()
+    const input = {
+      ...defaults,
+      ...data,
+    }
+    const app = wrap(new entities.App(references.user)).assign(input, { em })
+    em.persist(app)
+    return app
+  },
+}
+
+export { userHelper, jobHelper, appHelper }
