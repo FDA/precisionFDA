6eb893fdbfe4bdb565098a52c739f01ec2fc8bd8
Karolina Behalova <kbehalova-cf@dnanexus.com>
2021-01-11 11:15:38 +0100

    add: files deletion

--- https-apps-api/packages/shared/src/config/envs/test.ts
+++ https-apps-api/packages/shared/src/config/envs/test.ts
@@ -9,6 +9,7 @@ export const config: ConfigOverride = {
     dbName: process.env.DATABASE_TEST_NAME ?? 'precisionfda-test',
     clientUrl:
       process.env.DATABASE_TEST_URL ?? 'mysql://root:password@localhost:3306/precisionfda-test',
+    // debug: true,
     debug: false,
   },
   workerJobs: {
