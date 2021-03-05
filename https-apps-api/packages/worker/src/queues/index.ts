05e1f7941667a537772ef299d3949b9425266e00
Karolina Behalova <kbehalova@gmail.com>
2021-03-05 12:50:19 +0100

    Revert "fix: better logging of redis connection status"
    This reverts commit 719f48da2e03714ff38ae565fa3086a71bdd044f.

--- https-apps-api/packages/worker/src/queues/index.ts
+++ https-apps-api/packages/worker/src/queues/index.ts
@@ -1,11 +1,9 @@
 import { queue } from '@pfda/https-apps-shared'
 import { handler } from '../jobs'
-import { log } from '../utils'
 
 // starts all the queues, defined in shared, attaches the handlers
 const setupHandlers = async (): Promise<void> => {
-  await queue.createQueues()
-  log.info({ queueStatus: queue.getQueue().client.status }, 'https-apps-worker-queue status')
+  queue.createQueues()
   await queue.getQueue().process(handler)
 }
 
