76f6b78c11cbf0c8e2a000606b04d36e418b0457
Zai Chang <zchang-cf@dnanexus.com>
2021-03-21 14:52:35 +0800

    • User logged in as the expert can access expert edit and dashboard pages • Fixed blog preview not being displayed (N.B. it is stored as ‘_challenge’ in the meta column of the experts tables) • Circle icon for small expert list

--- app/serializers/expert_serializer.rb
+++ app/serializers/expert_serializer.rb
@@ -11,14 +11,15 @@ class ExpertSerializer < ApplicationSerializer
     :title,
     :about,
     :blog_title,
+    :blog_preview,
     :blog,
+    :total_question_count,
     :total_answer_count,
     :total_comment_count,
   )
 
-  def title
-    object.title
-  end
+  delegate :title, to: :object
+  delegate :total_comment_count, to: :object
 
   def about
     object._about
@@ -28,15 +29,24 @@ class ExpertSerializer < ApplicationSerializer
     object._blog_title
   end
 
+  # blog_preview - A short preview text of the blog to be displayed in a list as a preview.
+  #                Changing the attribute name from _challenge (which is how it is stored
+  #                in the 'meta' column) to avoid confusion to the consumers of the API
+  #
+  def blog_preview
+    object._challenge
+  end
+
+  # blog - the main body of the blog
   def blog
     object._blog
   end
 
-  def total_answer_count
+  def total_question_count
     object.answered_questions.count + object.ignored_questions.count + object.open_questions.count
   end
 
-  def total_comment_count
-    object.total_comment_count
+  def total_answer_count
+    object.answered_questions.count
   end
 end
