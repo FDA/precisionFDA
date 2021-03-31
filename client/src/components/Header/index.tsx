fbfb7ecfa0203d59cf2c765c37243b505c1b8aa1
David Cech <dcech-cf@dnanexus.com>
2021-03-31 18:48:15 +0200

    feat: styling header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 import { Logo } from '../Logo'
 import { Avatar } from '../Avatar'
 import { PageContainer } from '../Page/styles'
-import { HeaderIcon, HeaderLeft, HeaderRight, HeaderSpacer, Nav, StyledHeader } from './styles'
+import { HeaderItem, HeaderItemText, HeaderLeft, HeaderRight, HeaderSpacer, Nav, StyledHeader, StyledSupport, StyledUsername } from './styles'
+import { HomeIcon } from '../icons/HomeIcon'
+import { CommentIcon } from '../icons/CommentIcon'
+import { TrophyIcon } from '../icons/TrophyIcon'
+import { StarIcon } from '../icons/StarIcon'
+import { StickyNoteIcon } from '../icons/StickyNote'
+import { BullsEyeIcon } from '../icons/BullsEyeIcon'
+import { FortIcon } from '../icons/FortIcon'
+import { ObjectGroupIcon } from '../icons/ObjectGroupIcon'
+import { CommentingIcon } from '../icons/CommentingIcon'
+import { CaretIcon } from '../icons/CaretIcon'
 
 export const Header = () => {
   return (
@@ -11,25 +21,51 @@ export const Header = () => {
         <Nav>
           <HeaderLeft>
             <Logo />
-            <HeaderIcon>Overview</HeaderIcon>
-            <HeaderIcon>Discussions</HeaderIcon>
-            <HeaderIcon>Challenges</HeaderIcon>
-            <HeaderIcon>Experts</HeaderIcon>
+            <HeaderItem active={true}>
+              <HomeIcon />
+              <HeaderItemText>Overview</HeaderItemText>
+            </HeaderItem>
+            <HeaderItem>
+              <CommentIcon />
+               <HeaderItemText>Discussions</HeaderItemText>
+            </HeaderItem>
+            <HeaderItem>
+              <TrophyIcon />
+               <HeaderItemText>Challenges</HeaderItemText>
+            </HeaderItem>
+            <HeaderItem>
+              <StarIcon />
+               <HeaderItemText>Experts</HeaderItemText>
+            </HeaderItem>
             <HeaderSpacer />
-            <HeaderIcon>Notes</HeaderIcon>
-            <HeaderIcon>Comparisons</HeaderIcon>
+            <HeaderItem>
+              <StickyNoteIcon />
+               <HeaderItemText>Notes</HeaderItemText>
+            </HeaderItem>
+            <HeaderItem>
+              <BullsEyeIcon />
+               <HeaderItemText>Comparisons</HeaderItemText>
+            </HeaderItem>
             <HeaderSpacer />
-            <HeaderIcon>My Home</HeaderIcon>
+            <HeaderItem>
+              <FortIcon />
+               <HeaderItemText>My Home</HeaderItemText>
+            </HeaderItem>
             <HeaderSpacer />
-            <HeaderIcon>Spaces</HeaderIcon>
+            <HeaderItem>
+              <ObjectGroupIcon />
+               <HeaderItemText>Spaces</HeaderItemText>
+            </HeaderItem>
           </HeaderLeft>
           <HeaderRight>
-            <div>
+            <StyledSupport>
+              <CommentingIcon />
               <a href="mailto:precisionfda-support@dnanexus.com" target="_blank">Support</a>
-            </div>
-            <div>
+            </StyledSupport>
+            <StyledUsername>
               Username
-            </div>
+              <CaretIcon />
+            </StyledUsername>
             <Avatar />
           </HeaderRight>
         </Nav>
--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -2,4 +2,5 @@ export const theme = {
   primaryLite: '#7891b7',
   primary: '#1F70B5',
   primaryShade: '#185589',
+  white: '#F4F8FD',
 }
