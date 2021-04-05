e8b24f31686fac9f89d6155fc3e9c355c36fe92d
Behalka <kbehalova@gmail.com>
2021-04-05 18:13:57 +0200

    fix: different queue names (#1370)
    Co-authored-by: PT <ptater@dnanexus.com>

--- https-apps-api/packages/worker/src/queues/index.ts
+++ https-apps-api/packages/worker/src/queues/index.ts
@@ -1,11 +1,14 @@
-import { queue } from '@pfda/https-apps-shared'
+import { queue, config } from '@pfda/https-apps-shared'
 import { handler } from '../jobs'
 import { log } from '../utils'
 
 // starts all the queues, defined in shared, attaches the handlers
 const setupHandlers = async (): Promise<void> => {
   await queue.createQueues()
-  log.info({ queueStatus: queue.getQueue().client.status }, 'https-apps-worker-queue status')
+  log.info(
+    { queueStatus: queue.getQueue().client.status },
+    `${config.workerJobs.queues.default.name} status`,
+  )
   await queue.getQueue().process(handler)
 }
 
