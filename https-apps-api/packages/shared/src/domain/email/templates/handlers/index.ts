059df8d332456409eaddfa043d66ec1673af42cf
Karolina Behalova <kbehalova@gmail.com>
2021-04-15 12:05:53 +0200

    add: space change email handler

--- https-apps-api/packages/shared/src/domain/email/templates/handlers/index.ts
+++ https-apps-api/packages/shared/src/domain/email/templates/handlers/index.ts
@@ -3,3 +3,5 @@ export { JobFinishedEmailHandler } from './job-finished.handler'
 export { ContentChangedEmailHandler } from './content-change.handler'
 
 export { MemberChangedEmailHandler } from './member-change.handler'
+
+export { SpaceChangedEmailHandler } from './space-change.handler'
