9343e5e437b20ab281c7dfae2c71932487240acb
Aleksandr Moroz <amoroz-cf@dnanexus.com>
2021-09-02 23:26:40 +0300

    Cookbooks: id_rsa key deploy user owner.

--- chef/cookbooks/pfda/recipes/configure_ssh.rb
+++ chef/cookbooks/pfda/recipes/configure_ssh.rb
@@ -9,6 +9,8 @@ end
 
 file node[:ssh_key_path] do
   content lazy { node.run_state["app"]["app_source"]["ssh_key"] }
+  owner node[:deploy_user]
+  group node[:deploy_user]
   mode 0600
 end
 
@@ -16,5 +18,6 @@ template node[:ssh_wrapper_path] do
   source "wrap-ssh4git.sh.erb"
   variables key_path: node[:ssh_key_path]
   owner node[:deploy_user]
+  group node[:deploy_user]
   mode 0700
 end
