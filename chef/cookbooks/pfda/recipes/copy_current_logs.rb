3cbaf3e336e098b0a3d105de508b34d93ff9d911
Aleksandr Moroz <amoroz-cf@dnanexus.com>
2021-10-27 19:39:33 +0300

    PFDA-2762: get aws region from instance metadata.
    Move ssm parameters fetching to a single place.

--- chef/cookbooks/pfda/recipes/copy_current_logs.rb
+++
@@ -1,9 +0,0 @@
-template "/tmp/copy_current_logs.sh" do
-  backup false
-  source "copy_current_logs.sh.erb"
-  mode "0755"
-end
-
-execute "copy current logs" do
-  command "sh /tmp/copy_current_logs.sh"
-end
