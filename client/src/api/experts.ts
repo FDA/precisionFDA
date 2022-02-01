345f08bcd4743455854c2dd3324d0dc4dd7f90e6
Aleksei Ivanishchev <aivanishchev-cf@dnanexus.com>
2022-02-01 18:08:44 +0300

    PFDA-2921_Redesign_Expert_Blog_Post (#1684)
    * PFDA-2921_Redesign_Expert_Blog_Post
    
     - blog title and date issues fix
    
    * - pronto fix
    
    * - eslint fix
    
    * - eslint fix 2
    
    * - eslint IDE fix
    
    * - eslint fix
    
    * - update expert title & date display
    
    * - update tags content values
     - eslint fix

--- client/src/api/experts.ts
+++ client/src/api/experts.ts
@@ -1,13 +1,14 @@
 import { backendCall } from '../utils/api'
 import { queryYearList } from './yearList'
-import { IExpert } from '../types/expert'
+import { IExpert, mapToExpert } from '../types/expert'
 
 
 const getExperts = (data: any) => backendCall('/api/experts', 'GET', data)
 const fetchExpertDetails = async (expertId: string): Promise<IExpert> => {
   const res = await backendCall(`/api/experts/${expertId}`, 'GET')
+  const expert = [res?.payload.expert]
 
-  return res?.payload
+  return expert.map(mapToExpert)[0]
 }
 const askQuestion = (data: { userName: string, question: string }, expertId: string) => backendCall(`/api/experts/${expertId}/ask_question`, 'POST', data)
 
