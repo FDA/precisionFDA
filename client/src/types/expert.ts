ec83634040c0b30e6ff8fc4de2363dacb687f01b
Samuel Melkovic <smelkovic-cf@dnanexus.com>
2022-02-02 18:05:02 +0100

    Bugfixes

--- client/src/types/expert.ts
+++ client/src/types/expert.ts
@@ -17,7 +17,26 @@ interface IExpert extends IListItem {
   totalCommentCount: number,
 }
 
-const mapToExpert = (data: any) => ({
+const mapToExpert = (data: any) => {
+  return {
+    id: data.id,
+    user_id: data.user_id,
+    image: data.image,
+    state: data.state,
+    scope: data.scope,
+    createdAt: new Date(data.created_at),
+    updatedAt: new Date(data.updated_at),
+    title: data.title,
+    about: data.about,
+    blog: data.blog,
+    blogTitle: data.blog_title,
+    blogPreview: data.blog_preview,
+    totalAnswerCount: data.total_answer_count,
+    totalCommentCount: data.total_comment_count,
+  }
+}
+
+const mapToExpertNodeApi = (data: any) => ({
   id: data.id,
   user_id: data.user_id,
   image: data.image,
@@ -35,4 +54,4 @@ const mapToExpert = (data: any) => ({
 });
 
 export type { IExpert }
-export { mapToExpert }
+export { mapToExpertNodeApi, mapToExpert }
