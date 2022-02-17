f603e31d22ce5931097dfc4480e5346812f9121b
Aleksandr Moroz <amoroz@dnanexus.com>
2022-02-17 23:00:04 +0300

    PFDA-3160: do not show public navbar for logged users.

--- client/src/views/pages/Challenges/ChallengeProposePage/index.js
+++ client/src/views/pages/Challenges/ChallengeProposePage/index.js
@@ -1,71 +1,66 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { withRouter } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import PublicLayout from '../../../layouts/PublicLayout'
 import NavigationBar from '../../../components/NavigationBar/NavigationBar'
 import ChallengeProposeForm from '../../../components/Challenges/ChallengeProposeForm'
 import ChallengesYearList from '../../../components/Challenges/ChallengesYearList'
 import CollapsibleMenu from '../../../components/CollapsibleMenu'
+import { contextUserSelector } from '../../../../reducers/context/selectors'
 import './style.sass'
 
 
-class ChallengeProposePage extends React.Component {
+const ChallengeProposePage = () => {
+  const history = useHistory()
+  const user = useSelector(contextUserSelector)
 
-  render() {
-    const { history } = this.props
+  const title = 'Challenges'
+  const subtitle = 'Advancing regulatory standards for bioinformatics, RWD, and AI, through community-sourced science.'
+  const sideMenuOptions = [
+    {
+      text: 'Currently Open',
+      onClick: () => history.push('/challenges'),
+    },
+    {
+      text: 'Upcoming',
+      onClick: () => history.push('/challenges'),
+    },
+    {
+      text: 'Propose a Challenge',
+      onClick: () => console.log('Propose a Challenge clicked'),
+    },
+  ]
 
-    const title = 'Challenges'
-    const subtitle = 'Advancing regulatory standards for bioinformatics, RWD, and AI, through community-sourced science.'
-    const sideMenuOptions = [
-      {
-        text: 'Currently Open',
-        onClick: () => history.push('/challenges'),
-      },
-      {
-        text: 'Upcoming',
-        onClick: () => history.push('/challenges'),
-      },
-      {
-        text: 'Propose a Challenge',
-        onClick: () => console.log('Propose a Challenge clicked'),
-      },
-    ]
+  return (
+    <PublicLayout>
+      <NavigationBar title={title} subtitle={subtitle} user={user} />
 
-    return (
-      <PublicLayout>
-        <NavigationBar title={title} subtitle={subtitle}/>
-        
-        <div className="challenge-propose-main-container">
-          <div className="left-column">
-            <h1 className="pfda-section-heading">Propose a Challenge</h1>
-            <p>Do you have an idea, an objective, a dataset, an algorithm, or any combination of the above that you would like to put in front of the precisionFDA expert community.</p>
-            <p>Feel free to let us know! We would love to discuss this with you.</p>
-          </div>
-          <div className="middle-column">
-            <ChallengeProposeForm />
-          </div>
-          <div className="right-column">
-            <CollapsibleMenu title="Challenges" options={sideMenuOptions} />
-            <hr />
-            <div className="pfda-subsection-heading">PREVIOUS CHALLENGES</div>
-            <ChallengesYearList setYearHandler={(year) => {history.push('/challenges/?year='+year)}} />
-          </div>
+      <div className="challenge-propose-main-container">
+        <div className="left-column">
+          <h1 className="pfda-section-heading">Propose a Challenge</h1>
+          <p>Do you have an idea, an objective, a dataset, an algorithm, or any combination of the above that you would like to put in front of the precisionFDA expert community.</p>
+          <p>Feel free to let us know! We would love to discuss this with you.</p>
         </div>
-      </PublicLayout>
-    )
-  }
+        <div className="middle-column">
+          <ChallengeProposeForm />
+        </div>
+        <div className="right-column">
+          <CollapsibleMenu title="Challenges" options={sideMenuOptions} />
+          <hr />
+          <div className="pfda-subsection-heading">PREVIOUS CHALLENGES</div>
+          <ChallengesYearList setYearHandler={(year) => {history.push('/challenges/?year='+year)}} />
+        </div>
+      </div>
+    </PublicLayout>
+  )
 }
 
 ChallengeProposePage.propTypes = {
   history: PropTypes.object,
 }
 
-ChallengeProposePage.defaultProps = {
-}
+export { ChallengeProposePage }
 
-export {
-  ChallengeProposePage,
-}
-
-export default withRouter(ChallengeProposePage)
+export default ChallengeProposePage
