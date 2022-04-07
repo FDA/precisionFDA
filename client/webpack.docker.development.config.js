ed4f56d09d6c0fa625ff05bf7edca5f4e6a5d496
Samuel Melkovič <samuel.melkovic@gmail.com>
2022-04-07 15:09:02 +0200

    [PFDA] Frontend build cache (#1751)
    * Client build cache
    
    * Removed console.log
    
    * lint fix in pipeline
    
    Co-authored-by: Samuel Melkovic <smelkovic-cf@dnanexus.com>

--- client/webpack.docker.development.config.js
+++ client/webpack.docker.development.config.js
@@ -13,4 +13,16 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  // TODO(samuel) cache profiling in case needed
+  // https://webpack.js.org/configuration/cache#cacheprofile
+  cache: {
+    type: 'filesystem',
+    name: 'PfdaBuildCache',
+    cacheDirectory: path.resolve(__dirname, '.build_cache'),
+    buildDependencies: {
+      // eslint-disable-next-line no-undef
+      config: [__filename],
+    },
+    version: 'docker',
+  },
 })
