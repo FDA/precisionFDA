feb1c6ced508792e76c7f18c4863ccbac35711c4
Samuel Melkovič <samuel.melkovic@gmail.com>
2022-05-30 20:25:19 +0200

    Bugfix - Fix webpack sourcemaps (#1826)
    * Fix annoying things with webpack
    
    * CR Comments
    
    Co-authored-by: Samuel Melkovic <smelkovic-cf@dnanexus.com>
    Co-authored-by: Dave <dcech-cf@dnanexus.com>

--- client/webpack.fragment.swc.js
+++ client/webpack.fragment.swc.js
@@ -2,8 +2,12 @@
 
 const path = require('path')
 
+// NOTE(samuel) reason for exporting function instead of normal config is
+// failure of `yarn server` command without options.parseMap setting
+// also when options.parseMap is enabled "yarn docker:build" fails
+// Therefore we need to consider two versions of swc config for webpack
 
-module.exports = {
+module.exports = ({ enableSourceMaps } = { enableSourceMaps: false }) => ({
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, '../app/assets/packs/'),
@@ -16,11 +20,15 @@ module.exports = {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
         loader: 'swc-loader',
+        ...(enableSourceMaps ? {
+          options: {
+            parseMap: true,
+          },
+        } : {}),
       },
     ],
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
   },
-}
-
+})
