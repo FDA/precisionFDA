18044dfbbb043e8e1d7069af9bbfd2a3975ecb84
Samuel Melkovič <samuel.melkovic@gmail.com>
2022-06-09 16:05:00 +0200

    xD (#1853)
    Co-authored-by: Samuel Melkovic <smelkovic-cf@dnanexus.com>

--- https-apps-api/packages/shared/src/config/envs/index.ts
+++ https-apps-api/packages/shared/src/config/envs/index.ts
@@ -6,4 +6,6 @@ export { config as test } from './test'
 
 export { config as staging } from './staging'
 
+export { config as dev } from './dev'
+
 export { config as production } from './production'
