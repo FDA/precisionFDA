120b976e071062642c34946b09c96c50b4e28e82
Zai Chang <zchang-cf@dnanexus.com>
2022-08-12 12:37:42 +0100

    Further console.log removal

--- https-apps-api/packages/worker/test/index.ts
+++ https-apps-api/packages/worker/test/index.ts
@@ -25,30 +25,20 @@ chai.use(chaiAsPromised)
 chai.use(dirtyChai)
 
 before(async () => {
-  console.log('before: mocksSetup')
   mocksSetup()
-  console.log('before: localMocksSetup')
   localMocksSetup()
 
-  console.log('before: await database.start()')
   await database.start()
-  console.log('before: await setupHandlers()')
   await setupHandlers()
-  console.log('before: await db.initDeleteProcedure(database.connection())')
   await db.initDeleteProcedure(database.connection())
-  console.log('before: end')
 })
 
 after(async () => {
-  console.log('after: localMocksRestore()')
   localMocksRestore()
-  console.log('after: mocksRestore()')
   mocksRestore()
   // TODO(samuel) solve this somehow
   // uncommenting throws timeout errors, failed to investigate why, as it works locally
   // shouldn't impact test results as redis communication is mocked
   // await queue.disconnectQueues()
-  console.log('after: await database.stop()')
   await database.stop()
-  console.log('after: end')
 })
