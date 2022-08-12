087e3c89b4052b96545365d8e09fc0254bf4f9a2
Zai Chang <zchang-cf@dnanexus.com>
2022-08-12 11:21:42 +0100

    Added fakes.removeRepeatableStub

--- https-apps-api/packages/worker/test/utils/mocks.ts
+++ https-apps-api/packages/worker/test/utils/mocks.ts
@@ -12,21 +12,24 @@ const fakes = {
   }),
   removeJobsStub: sinon.stub(),
   // Stubbing getRepeatableJobs to avoid jobs clearing code crashing during tests
-  getRepeatableJobsStub: sinon.stub().callsFake(async () => {
+  getRepeatableJobsStub: sinon.stub().callsFake(() => {
     return []
   }),
+  removeRepeatableStub: sinon.stub(),
 }
 
 const mocksSetup = () => {
   sandbox.replace(Bull.prototype, 'add', fakes.addToQueueStub)
   sandbox.replace(Bull.prototype, 'removeJobs', fakes.removeJobsStub)
   sandbox.replace(Bull.prototype, 'getRepeatableJobs', fakes.getRepeatableJobsStub)
+  sandbox.replace(Bull.prototype, 'removeRepeatable', fakes.removeRepeatableStub)
 }
 
 const mocksReset = () => {
   fakes.addToQueueStub.resetHistory()
   fakes.removeJobsStub.resetHistory()
   fakes.getRepeatableJobsStub.resetHistory()
+  fakes.removeRepeatableStub.resetHistory()
 }
 
 const mocksRestore = () => {
