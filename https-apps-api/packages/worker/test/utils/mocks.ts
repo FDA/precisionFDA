e610c15c447e1f0ba6d992fab0e6b7797e1e5d88
Zai Chang <zchang-cf@dnanexus.com>
2022-08-12 11:58:11 +0100

    getJobCounts fake

--- https-apps-api/packages/worker/test/utils/mocks.ts
+++ https-apps-api/packages/worker/test/utils/mocks.ts
@@ -10,25 +10,28 @@ const fakes = {
   addToQueueStub: sinon.stub().callsFake(async input => {
     await handler({ data: input } as Job)
   }),
-  removeJobsStub: sinon.stub(),
+  getJobCountsStub: sinon.stub(),
   // Stubbing getRepeatableJobs to avoid jobs clearing code crashing during tests
   getRepeatableJobsStub: sinon.stub().callsFake(() => {
     return []
   }),
+  removeJobsStub: sinon.stub(),
   removeRepeatableStub: sinon.stub(),
 }
 
 const mocksSetup = () => {
   sandbox.replace(Bull.prototype, 'add', fakes.addToQueueStub)
-  sandbox.replace(Bull.prototype, 'removeJobs', fakes.removeJobsStub)
+  sandbox.replace(Bull.prototype, 'getJobCounts', fakes.getJobCountsStub)
   sandbox.replace(Bull.prototype, 'getRepeatableJobs', fakes.getRepeatableJobsStub)
+  sandbox.replace(Bull.prototype, 'removeJobs', fakes.removeJobsStub)
   sandbox.replace(Bull.prototype, 'removeRepeatable', fakes.removeRepeatableStub)
 }
 
 const mocksReset = () => {
   fakes.addToQueueStub.resetHistory()
-  fakes.removeJobsStub.resetHistory()
+  fakes.getJobCountsStub.resetHistory()
   fakes.getRepeatableJobsStub.resetHistory()
+  fakes.removeJobsStub.resetHistory()
   fakes.removeRepeatableStub.resetHistory()
 }
 
