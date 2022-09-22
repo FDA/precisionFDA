ee021d1b0b9a41f299ac0671ff38ca57bfe2e2b5
Zai Chang <zchang-cf@dnanexus.com>
2022-09-22 10:18:00 +0200

    PFDA-3427: Upgrade Chef to 17.10.3 (#1985)
    * PFDA-3427: Upgrade Chef to 17.10.3
    - Removed uses of poise-application and poise-service
    - Also changed ‘aws s3 cp’ to ‘aws s3 sync’ for ginas.ix
    
    * - Adding action :start
    
    * - systemd_units
    - attempt on environment.conf
    
    * Fix copy-paste error
    
    * - writing template files as root
    
    * - gsrs template
    
    * - gsrs and ruby recipes getting closer
    
    * - Friday’s final WIP
    
    * - Specifying everywhere that this unit is to be run under user deploy
    - Testing what happens if we don’t nuke gsrs_path
    
    * - Abandoning systemctl --user for now and using system
    - Renamed sidekiq.service.erb to pfda-sidekiq.service.erb
    
    * - Fix for deploy_ruby.rb
    
    * - Sidekiq fixes, hopefully
    
    * - Added WorkingDirectory
    
    * - Fixed RUBY_ENV missing in pfda-puma.service
    
    * - Formatting
    
    * - Workaround for running systemctl as root
    
    * - Fixed sidekiq, same issue as the #{ENV['RAILS_ENV']} issue with puma
    
    * - Delete pfda-sidekiq.service.erb as the definition is now inline in deploy_ruby.rb
    - Amended README

--- chef/cookbooks/pfda/metadata.rb
+++ chef/cookbooks/pfda/metadata.rb
@@ -6,8 +6,7 @@ license "All Rights Reserved"
 description "Installs/Configures pfda"
 long_description "Installs and configures pFDA server"
 version "0.1.0"
-chef_version "~> 16.0" if respond_to?(:chef_version)
+chef_version "~> 17.10.3" if respond_to?(:chef_version)
 
-depends "application", "~> 5.2.0"
 depends "aws", "~> 8.4.0"
 depends "nodejs", "~> 7.3.2"
