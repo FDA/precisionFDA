100fcb0daab02690693d67f2da3fa4c220810635
olgakarpacz <104008132+olgakarpacz@users.noreply.github.com>
2022-11-23 08:57:39 +0100

    PFDA-3438: SPACES - Data sorting is not working (#1976)
    * set url params added
    
    * Fixed space_type and state sorting, disabled others
    
    * updated sorting and filters
    
    * enabled filtering by leads | WIP
    
    * disabled leads filters on frontend
    
    * re-enabled filters for space leads
    
    * fix: remove filter key from url
    
    * filter changes
    
    * rubocop changes
    
    * removed leads filter completely
    
    * useless vars removed
    
    * fixed tags filtering in spaces
    
    * fixed private space type label
    
    Co-authored-by: “olgakarpacz” <“okorpacheva@dnanexus.com”>
    Co-authored-by: Jiří Lagan <jlagan@dnanexus.com>
    Co-authored-by: David Cech <dcech-cf@dnanexus.com>
    Co-authored-by: Jiří Lagan <43816355+jirkalagan@users.noreply.github.com>

--- app/services/space_service/spaces_filter.rb
+++ app/services/space_service/spaces_filter.rb
@@ -22,7 +22,6 @@ module SpaceService
           )
 
       spaces = spaces.where(build_where(filters)).joins(:users) if filters.present?
-
       spaces
     end
 
@@ -33,12 +32,15 @@ module SpaceService
       conditions = []
       space_arel = Space.arel_table
       space_type = Space.space_types[filters[:type]]
+      space_state = Space.states[filters[:state]]
 
       conditions << space_arel[:space_type].eq(space_type) if space_type
+      conditions << space_arel[:state].eq(space_state) if space_state
       conditions << space_arel[:name].matches(wildcard(filters[:name])) if filters[:name]
       conditions << space_arel[:created_at].matches(wildcard(filters[:created_at])) if filters[:created_at]
       conditions << space_arel[:updated_at].matches(wildcard(filters[:updated_at])) if filters[:updated_at]
       conditions << space_arel[:description].matches(wildcard(filters[:description])) if filters[:description]
+
       conditions.reduce(nil) do |where, condition|
         where ? where.and(condition) : condition
       end
