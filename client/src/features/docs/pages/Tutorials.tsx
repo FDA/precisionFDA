ebaf26addc893eeec7d6fec248b6ac9e88820032
Jiří Lagan <43816355+jirkalagan@users.noreply.github.com>
2023-01-05 09:16:06 +0100

    PFDA-3969 fixed typo in tutorial page (#2121)

--- client/src/features/docs/pages/Tutorials.tsx
+++ client/src/features/docs/pages/Tutorials.tsx
@@ -7,26 +7,26 @@ export const Tutorials = () => (
     <DocBody>
       <h1>Tutorials</h1>
       Click the following links to download the most up-to-date tutorials on how to use PrecisionFDA.
-      
+
       <ul>
         <li>
           <a
-            href="/pdfs/Tuturial_-_Apps_and_Workflows_-_20221130.pdf"
+            href="/pdfs/Tutorial_-_Apps_and_Workflows_-_20221130.pdf"
             data-turbolinks="false"
             target="_blank"
             aria-label="Tutorial on apps and workflows"
           >
-            Tuturial - Apps and Workflows.pdf
+            Tutorial - Apps and Workflows.pdf
           </a>
         </li>
         <li>
           <a
-            href="/pdfs/Tuturial_-_Workstations_and_Databases_-_20221130.pdf"
+            href="/pdfs/Tutorial_-_Workstations_and_Databases_-_20221130.pdf"
             data-turbolinks="false"
             target="_blank"
             aria-label="Tutorial on workstations and databases"
           >
-            Tuturial - Workstations and Databases.pdf
+            Tutorial - Workstations and Databases.pdf
           </a>
         </li>
       </ul>
