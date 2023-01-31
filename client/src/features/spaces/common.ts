cc6fcc5350abdcfb3bf3e36805e4643a0abd9d1a
olgakarpacz <104008132+olgakarpacz@users.noreply.github.com>
2023-01-31 17:18:32 +0100

    PFDA-4040: FILE LOCKING issues (#2152)
    * file locking issues
    
    * scope added
    
    * eslint fix
    
    * based on role disable action
    
    * fix for space

--- client/src/features/spaces/common.ts
+++ client/src/features/spaces/common.ts
@@ -11,7 +11,8 @@ export const SpaceTypeName = {
 export const getHostLeadLabel = (type: ISpace['type']) => {
   if (type === 'review') {
     return 'Reviewer Lead'
-  } if (type === 'groups') {
+  }
+  if (type === 'groups') {
     return 'Host Lead'
   }
   return ''
@@ -20,24 +21,41 @@ export const getHostLeadLabel = (type: ISpace['type']) => {
 export const getGuestLeadLabel = (type: ISpace['type']) => {
   if (type === 'review') {
     return 'Reviewer Lead'
-  } if (type === 'groups') {
+  }
+  if (type === 'groups') {
     return 'Guest Lead'
   }
   return ''
 }
 
 /**
- * Disable action if space is protected and current user is not lead.
+ * Disable action if current user is not a space lead.
  */
-export const isActionDisabledBasedOnProtected = (authUserId?: number, space?: ISpace) => {
+export const isActionDisabledBasedOnRole = (
+  authUserId?: number,
+  space?: ISpace,
+) => {
   if (!space) {
     return false
   }
-  if (!space.protected) {
-    return false
-  }
-  if (space.host_lead?.id === authUserId || space.guest_lead?.id === authUserId) {
+  if (
+    space.host_lead?.id === authUserId ||
+    space.guest_lead?.id === authUserId
+  ) {
     return false
   }
   return true
+}
+
+/**
+ * Disable action if space is protected and current user is not lead.
+ */
+export const isActionDisabledBasedOnProtected = (
+  authUserId?: number,
+  space?: ISpace,
+) => {
+  if (!space?.protected) {
+    return false
+  }
+  return isActionDisabledBasedOnRole(authUserId, space)
 }
