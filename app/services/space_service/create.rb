272b197ad179a6a4e536947c289f7e44ac312f56
Jiří Lagan <43816355+jirkalagan@users.noreply.github.com>
2023-03-07 17:27:26 +0100

    PFDA-3748 admin spaces - reduced # of emails being sent (#2172)
    * PFDA-3748 reduced # of emails being sent
    
    * correct response status code fixed for smtp
    
    * added more logs
    
    * first commit returned
    
    * removed additional logging
    
    * changed space activated email recipient
    
    * added testing email params
    
    * for testing purposes
    
    * reverted after testing
    
    * nodemailer types updated
    
    * removed unused type
    
    * removed additional formatting
    
    ---------
    
    Co-authored-by: olgakarpacz <okorpacheva@dnanexus.com>
    Co-authored-by: olgakarpacz <104008132+olgakarpacz@users.noreply.github.com>
    Co-authored-by: mvojtisek-cf <90391755+mvojtisek-cf@users.noreply.github.com>

--- app/services/space_service/create.rb
+++ app/services/space_service/create.rb
@@ -71,9 +71,9 @@ module SpaceService
         Rails.logger.info("Adding site admin #{user.dxuser} to space #{space.id}" \
                           " with admin membership #{admin_membership.id}")
         SpaceMembershipService::CreateOrUpdate.call(api, space, site_admin, SpaceMembership::ROLE_ADMIN, admin_membership, false)
-        NotificationsMailer.space_activated_email(space, admin_membership).deliver_later!
         # rubocop:enable Layout/LineLength
       end
+      NotificationsMailer.space_activated_email(space, admin_membership).deliver_later!
     end
 
     # Sends invitation emails to administrator space
--- https-apps-api/packages/shared/src/services/smtp.service.ts
+++ https-apps-api/packages/shared/src/services/smtp.service.ts
@@ -1,17 +1,9 @@
-import * as nodemailer from 'nodemailer'
-import { errors } from '..'
-import { config } from '../config'
-import { SendEmailJob } from '../queue/task.input'
-import { getLogger } from '../logger'
-import SMTPTransport from 'nodemailer/lib/smtp-transport'
-
-type SendEmailResponse = {
-  success: string
-  errors?: {
-    message: string
-    statusCode: string
-  }
-}
+import * as nodemailer from 'nodemailer';
+import { errors } from '..';
+import { config } from '../config';
+import { SendEmailJob } from '../queue/task.input';
+import { getLogger } from '../logger';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 const log = getLogger('nodemailer-logger')
 
@@ -19,15 +11,15 @@ class EmailClient {
   transporter: nodemailer.Transporter
 
   constructor() {
-    const transportConfig: SMTPTransport.Options = {
+    const transportConfig : SMTPTransport.Options = {
       host: config.emails.smtp.host,
-      port: parseInt(config.emails.smtp.port),
+      port: parseInt(config.emails.smtp.port) || 0,
       secure: true,
       auth: {
         user: config.emails.smtp.username,
         pass: config.emails.smtp.password,
       },
-    }
+    };
 
     // Comment out the following to debug SMTP connection
     // transportConfig.auth.pass = '[masked]'
@@ -38,36 +30,37 @@ class EmailClient {
     this.transporter = nodemailer.createTransport(transportConfig)
     this.transporter.verify((error, success) => {
       if (success) {
-        log.info({ success }, 'SMTP connection configuration is successful')
+        log.info({ success }, 'SMTP connection configuration is successful');
       }
       if (error) {
-        log.error({ error }, 'SMTP connection configuration failed')
+        log.error({ error }, 'SMTP connection configuration failed');
       }
-    })
+    });
   }
 
   async sendEmail(input: SendEmailJob['payload']): Promise<void> {
-    // eslint-disable-next-line no-underscore-dangle
-    const res: SendEmailResponse = await this.transporter.sendMail({
-      from: config.emails.smtp.fromAddress,
-      cc: [],
-      to: input.to,
-      subject: input.subject,
-      html: input.body,
-    })
-    if (res.success === 'true') {
-      log.info({ res }, 'SMTP request successful')
-    }
-    if (res.success !== 'true') {
-      log.error({ error: res.errors }, 'SendEmail failed')
-      throw new errors.ServiceError('SMTP request failed', {
-        code: errors.ErrorCodes.AWS_SES_SERVICE_ERROR,
-        clientResponse: res.success,
-        // unknown
-        clientStatusCode: 400,
-      })
-    }
-    log.info({ res }, 'SMTP request successful')
+    return await this.transporter.sendMail(
+      {
+        from: config.emails.smtp.fromAddress,
+        cc: [],
+        to: input.to,
+        subject: input.subject,
+        html: input.body,
+      },
+      (error, info) => {
+        if (info) {
+          log.info({ info }, 'SMTP request successful');
+        }
+        if (error) {
+          log.error({ error: error }, 'SendEmail failed');
+          throw new errors.ServiceError('SMTP request failed', {
+            code: errors.ErrorCodes.AWS_SES_SERVICE_ERROR,
+            clientResponse: error.message,
+            clientStatusCode: 424,
+          });
+        }
+      }
+    );
   }
 }
 
