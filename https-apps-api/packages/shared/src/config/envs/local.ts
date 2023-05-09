ee242521e6e606cf2f4ad9c970f4e77b46e9a733
olgakarpacz <104008132+olgakarpacz@users.noreply.github.com>
2023-05-09 16:53:37 +0200

    PFDA-3842: On localhost save e-mails on disk (#2252)
    * local emails added to factory service
    
    * smtp is enabled removed
    
    * config fixed
    
    * interface fixed
    
    * service dependency fixed
    
    * unused config removed
    
    * redundant class removed
    
    * method fixed
    
    * config fixed
    
    * removed unused code
    
    * email type added to the file name
    
    * conflict resolved
    
    * tiny fixes

--- https-apps-api/packages/shared/src/config/envs/local.ts
+++ https-apps-api/packages/shared/src/config/envs/local.ts
@@ -14,7 +14,7 @@ export const config: ConfigOverride = () => ({
   },
   emails: {
     smtp: {
-      isEnabled: false,
+      saveEmailToFile: true,
     },
   },
   devFlags: {
@@ -35,8 +35,8 @@ export const config: ConfigOverride = () => ({
     },
     syncJob: {
       repeatPattern: '*/1 * * * *', // Every 1 minute
-      staleJobsEmailAfter: process.env.NODE_STALE_JOBS_EMAIL_AFTER ?? 60*2, // 2 minutes
-      staleJobsTerminateAfter: process.env.NODE_STALE_JOBS_TERMINATE_AFTER ?? 24*60*60*10, // 10 days
+      staleJobsEmailAfter: process.env.NODE_STALE_JOBS_EMAIL_AFTER ?? 60 * 2, // 2 minutes
+      staleJobsTerminateAfter: process.env.NODE_STALE_JOBS_TERMINATE_AFTER ?? 24 * 60 * 60 * 10, // 10 days
     },
     nonTerminatedDbClusters: {
       repeatPattern: process.env.NODE_NON_TERMINATED_DB_CLUSTERS_CRON ?? '0 6 * * *',
