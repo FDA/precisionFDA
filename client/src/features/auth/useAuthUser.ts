5c2df0baa77b4710f132cea28015109450b363c5
Pavel Kubal <pkubal@dnanexus.com>
2023-05-16 13:29:59 +0200

    PFDA-4290 ok, I give up

--- client/src/features/auth/useAuthUser.ts
+++ client/src/features/auth/useAuthUser.ts
@@ -7,7 +7,7 @@ export function useAuthUser(): IUser | undefined {
   const user: Partial<IUser> = {}
 
   const emailDomain = data?.user?.email?.split('@').pop()
-  user.isGovUser = emailDomain?.endsWith('fda.hhs.gov') || false
+  user.isGovUser = emailDomain === 'fda.hhs.gov'
   user.isAdmin = data?.user?.can_administer_site || false
   return data?.user === undefined ? undefined : ({ ...data.user, ...user, session_id: data?.meta.session_id })
 }
