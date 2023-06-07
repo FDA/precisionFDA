5cd7e7c82261244118fd4c836dfb6413a830b6df
Pavel Kubal <pkubal@dnanexus.com>
2023-06-07 10:16:47 +0200

    PFDA-4130 added heap dump on out of memory error (#2317)
    * PFDA-4130 added heapdump on out of memory error
    
    * PFDA-4130 use logger instead of console

--- https-apps-api/packages/api/src/index.ts
+++ https-apps-api/packages/api/src/index.ts
@@ -1,5 +1,12 @@
 import { createApp } from './server/app'
 import { createEntrypoint } from './entrypoint'
+import { writeHeapSnapshot } from 'v8'
+import { log } from './logger'
+
+process.on('SIGUSR2', () => {
+  const fileName = writeHeapSnapshot()
+  log.info(`Created heap dump file: ${fileName}`)
+})
 
 const app = createApp()
 createEntrypoint(() => app.callback())()
