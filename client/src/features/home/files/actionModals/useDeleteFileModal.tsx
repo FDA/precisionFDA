2a857400ba260ee4e33b56c6a02e019b6069503e
Petr Barta <pbarta@dnanexus.com>
2023-10-12 15:51:02 +0200

    Correct scope used when validating the files to be deleted; Delete validated files only; Delete button disabled until there is something to delete

--- client/src/features/home/files/actionModals/useDeleteFileModal.tsx
+++ client/src/features/home/files/actionModals/useDeleteFileModal.tsx
@@ -23,25 +23,31 @@ const StyledPath = styled.div`
 
 const DeleteFiles = ({
   selected,
-  scope,
   setNodesToBeDeleted,
 }: {
   selected: IFile[]
-  scope: string
   setNodesToBeDeleted: (nodes: DownloadListResponse[]) => void
 }) => {
   const { data, status } = useQuery(
     ['download_list', selected],
     () => {
-      // Workaround to use the correct 'scope' param when deleting single file
-      let usedScope = scope;
-      if (selected.length === 1) {
-        usedScope = selected[0].scope
+      // Group files by scope name
+      const filesByScopes = new Map<string, IFile[]>()
+      for (const file of selected) {
+        if (!filesByScopes.has(file.scope)) {
+          filesByScopes.set(file.scope, [])
+        }
+        filesByScopes.get(file.scope).push(file)
       }
-      return fetchFilesDownloadList(
-          selected.map(s => s.id),
-          usedScope,
-      )
+
+      const promises: Promise<DownloadListResponse[]>[] = []
+      for (let [scope, files] of filesByScopes) {
+        promises.push(fetchFilesDownloadList(
+            files.map(s => s.id),
+            scope,
+        ))
+      }
+      return Promise.all(promises).then(fileArrays => Promise.resolve(fileArrays.flat()));
     },
     {
       onSuccess: (res) => {
@@ -139,7 +145,7 @@ export const useDeleteFileModal = ({
   })
 
   const handleSubmit = () => {
-    mutation.mutateAsync(memoSelected.map(s => s.id))
+    mutation.mutateAsync(nodesToBeDeleted.map(s => s.id))
   }
 
   const modalComp = isShown && (
@@ -158,14 +164,14 @@ export const useDeleteFileModal = ({
           >
             Cancel
           </Button>
-          <ButtonSolidRed onClick={handleSubmit} disabled={mutation.isLoading}>
+          <ButtonSolidRed onClick={handleSubmit} disabled={!nodesToBeDeleted || mutation.isLoading}>
             Delete
           </ButtonSolidRed>
         </>
       }
     >
       <ModalScroll>
-        <DeleteFiles selected={memoSelected} scope={scope} setNodesToBeDeleted={setNodesToBeDeleted}/>
+        <DeleteFiles selected={memoSelected} setNodesToBeDeleted={setNodesToBeDeleted}/>
       </ModalScroll>
     </Modal>
   )
