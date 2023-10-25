adfc59c1119489f9033bceb37e0d4de03b6ca181
Jan Gabriel <jgabriel@dnanexus.com>
2023-10-25 22:35:57 +0200

    PFDA-4660 Staging-codebuild-failures-due-to-nodejs-binary-download (#2485)
    * init
    
    * cleanup

--- chef/roles/pfda_server.rb
+++ chef/roles/pfda_server.rb
@@ -1,7 +1,6 @@
 name "pfda_server"
 description "The role for pFDA application server"
 run_list "recipe[pfda::get_ssm_parameters]",
-         "recipe[pfda::setup_nodejs]",
          "recipe[pfda::setup_qualys_agent]",
          "recipe[pfda::configure_nginx]",
          "recipe[pfda::configure_https_app]",
