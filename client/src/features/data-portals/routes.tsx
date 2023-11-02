c3d8d558d554afdbd8e502305cfd64d279f9089c
Jiří Lagan <43816355+jirkalagan@users.noreply.github.com>
2023-11-02 16:55:04 +0100

    PFDA-4657 daaas sso redirect feature (#2492)
    * PFDA-4657 DAaaS SSO redirect for non-logged users
    
    * added sso redirect to daaas - WIP
    
    * removed console log
    
    * fix
    
    * fix 2
    
    * improvements
    
    * fix of redirect_uri
    
    * useAuthUser2 - WIP
    
    * useAuthUser enhancement
    
    * unused import
    
    * removed unnecesary semicolons
    
    * updated gov user condition
    
    * removed unnecesary optional chaining

--- client/src/features/data-portals/routes.tsx
+++ client/src/features/data-portals/routes.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import { Route, Switch, useRouteMatch } from 'react-router-dom'
+import NavigationBar, {
+  NavigationBarBanner,
+  NavigationBarPublicLandingTitle,
+} from '../../components/NavigationBar/NavigationBar'
+import { PageContainerMargin } from '../../components/Page/styles'
 import { usePageMeta } from '../../hooks/usePageMeta'
+import PublicLayout from '../../layouts/PublicLayout'
+import { Tagline } from '../../pages/Tagline'
+import { DataPortalsAuthPickerModal } from '../auth/DataPortalsAuthPickerModal'
+import { useAuthUser } from '../auth/useAuthUser'
+import { useModal } from '../modal/useModal'
 import DataPortalsListPage from './list/DataPortalsListPage'
 import EditDataPortalPage from './form/EditDataPortalPage'
 
@@ -23,6 +33,26 @@ const DataPortalResourcesPage = React.lazy(
 const DataPortalRoutes = () => {
   usePageMeta({ title: 'DAaaS - precisionFDA' })
   const { path } = useRouteMatch()
+  const { loading, user } = useAuthUser(true)
+
+  const modal = useModal()
+
+  if (!user && !loading ) {
+    // if accessed as non-logged user, present modal with landing-page background
+    return <PublicLayout>
+      <DataPortalsAuthPickerModal modal={modal}/>
+      <NavigationBar user={null}>
+        <PageContainerMargin>
+          <NavigationBarBanner>
+            <NavigationBarPublicLandingTitle>
+              <Tagline/>
+            </NavigationBarPublicLandingTitle>
+          </NavigationBarBanner>
+        </PageContainerMargin>
+      </NavigationBar>
+      <div style={{height: '100%'}}></div>
+    </PublicLayout>
+  }
 
   return (
     <Switch>
