64fe63524acb7318375753eb75bbd6ce445ea902
Petr Barta <146750460+bartapes@users.noreply.github.com>
2023-12-08 20:04:00 +0100

    Pfda 4850 bull board not working (#2555)
    Co-authored-by: Michal Vojtíšek <mvojtisek@dnanexus.com>

--- server/apps/api/src/index.ts
+++ server/apps/api/src/index.ts
@@ -2,11 +2,19 @@ import { createApp } from './server/app'
 import { createEntrypoint } from './entrypoint'
 import { writeHeapSnapshot } from 'v8'
 import { log } from './logger'
+import { database } from '@shared'
 
 process.on('SIGUSR2', () => {
   const fileName = writeHeapSnapshot()
   log.info(`Created heap dump file: ${fileName}`)
 })
 
-const app = createApp()
-createEntrypoint(() => app.callback())()
+database
+  .start()
+  .then(() => {
+    const app = createApp()
+    createEntrypoint(() => app.callback())()
+  })
+  .catch((e) => {
+    log.error(`Error while creating the database service: ${e.message}`)
+  })
