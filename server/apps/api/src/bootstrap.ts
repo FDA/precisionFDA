4c41b242972dd87b59bdd7bed8258c20752d1604
Pavel Kubal <pkubal@dnanexus.com>
2024-02-26 14:09:55 +0100

    PFDA-4954 cant edit portal page (#2698)
    Co-authored-by: “Ludvik <“lbobek@dnanexus.com”>

--- server/apps/api/src/bootstrap.ts
+++ server/apps/api/src/bootstrap.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestApplicationOptions } from '@nestjs/common/interfaces/nest-application-options.interface'
 import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
 import { setupNestApp } from '@shared/app-initialization'
 import { config } from '@shared/config'
 import fs from 'fs'
@@ -21,9 +22,9 @@ export async function bootstrap() {
     }
   }
 
-  const app = await NestFactory.create(ApiModule, options)
+  const app = await NestFactory.create<NestExpressApplication>(ApiModule, options)
   app.useWebSocketAdapter(new WebsocketAdapter(app))
-
+  app.useBodyParser('json', { limit: '16mb' })
   await setupNestApp(app)
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
