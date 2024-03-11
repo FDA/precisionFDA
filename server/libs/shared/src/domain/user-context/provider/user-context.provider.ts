c70595f0cfcd81743e423c66890748b1c5b8fd3b
Ludvik Bobek <139764338+lbobek-dnanexus@users.noreply.github.com>
2024-03-11 14:00:16 +0100

    Fix user context not having own properties (#2733)
    Co-authored-by: “Ludvik <“lbobek@dnanexus.com”>

--- server/libs/shared/src/domain/user-context/provider/user-context.provider.ts
+++ server/libs/shared/src/domain/user-context/provider/user-context.provider.ts
@@ -1,11 +1,11 @@
 import { Provider } from '@nestjs/common'
 import { UserContext } from '@shared/domain/user-context/model/user-context'
-import { UserContextStorageManager } from '../storage/user-context-storage.manager'
+import { createUserContextManager } from '../storage/user-context-storage.manager'
 import { userContextStorage } from '../storage/user-context.storage'
 
 export const userContextProvider: Provider = {
   provide: UserContext,
   useFactory: () => {
-    return new UserContextStorageManager(userContextStorage)
+    return createUserContextManager(userContextStorage)
   },
 }
