c8b5064378eedab9fc5a6c7b9ffe0009087842fb
Jiří Lagan <43816355+jirkalagan@users.noreply.github.com>
2024-06-03 09:20:28 +0200

    PFDA-5384 CLI ls show removing nodes (#2870)

--- packages/server/apps/api/src/cli/cli.controller.ts
+++ packages/server/apps/api/src/cli/cli.controller.ts
@@ -1,59 +1,22 @@
-import { SqlEntityManager } from '@mikro-orm/mysql'
-import { Body, Controller, Get, HttpCode, Inject, Logger, Param, Post } from '@nestjs/common'
-import {
-  DEPRECATED_SQL_ENTITY_MANAGER,
-} from '@shared/database/provider/deprecated-sql-entity-manager.provider'
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common'
+import { CliNodeSearchDTO } from '@shared/domain/cli/dto/CliNodeSearchDTO'
 import { CliService } from '@shared/domain/cli/service/cli.service'
-import { CLINodeSearchOperation } from '@shared/domain/user-file/ops/cli-node-search'
-import { CLINodeSearchInput, CLINodeSearchSchema } from '@shared/domain/user-file/user-file.input'
-import { UserOpsCtx } from '@shared/types'
-import { UserContext } from '@shared/domain/user-context/model/user-context'
-import { JsonSchemaPipe } from '../validation/pipes/json-schema.pipe'
 
 // SPECIAL ROUTES INTENDED FOR CLI USAGE ONLY. CONTAINS CLI SPECIFIC LOGIC & SPECIAL RESPONSE OBJECTS.
 @Controller('/cli')
 export class CliController {
-  constructor(
-    private readonly user: UserContext,
-    @Inject(DEPRECATED_SQL_ENTITY_MANAGER) private readonly em: SqlEntityManager,
-    private readonly log: Logger,
-    private readonly cliService: CliService,
-  ) {
-  }
+  constructor(private readonly cliService: CliService,) {}
 
-  // Finds all matching nodes and returns them.
   @HttpCode(200)
   @Post('/nodes')
-  async findNodes(
-    @Body(new JsonSchemaPipe(CLINodeSearchSchema))
-      body: CLINodeSearchInput,
-  ) {
-    const { spaceId, folderId, arg, type } = body
-
-    const opsCtx: UserOpsCtx = {
-      log: this.log,
-      user: this.user,
-      em: this.em,
-    }
-
-    const res = await new CLINodeSearchOperation(opsCtx).execute({
-      spaceId,
-      folderId,
-      arg,
-      type,
-    })
-
-    return {
-      files: res,
-      folders: [],
-    }
+  async findNodes(@Body() body: CliNodeSearchDTO) {
+    return this.cliService.findNodes(body)
   }
 
   @Get('/version/latest')
   getLatestVersion() {
-    return { version: '2.7.0' }
+    return { version: '2.7.1' }
   }
-
 
   @Get('/:uid/describe')
   async describeEntity(@Param('uid') uid: string) {
