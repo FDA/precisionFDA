2f0449f8aba37b26e7fa33777a15aab9d669e001
Pavel Kubal <pkubal@dnanexus.com>
2024-06-06 16:01:10 +0200

    PFDA-5412 Issues when selecting subfolders (#2885)

--- packages/client/src/features/files/file.utils.ts
+++ packages/client/src/features/files/file.utils.ts
@@ -4,8 +4,15 @@ import { DataNode } from 'rc-tree/lib/interface'
 export function findById(tree: DataNode[], nodeId: React.Key): DataNode | null {
   for (let i = 0; i < tree.length; i++) {
     const node = tree[i]
-    if (node.key === nodeId) return node
-    if (node.children) return findById(node.children, nodeId)
+    if (node.key === nodeId) {
+      return node
+    }
+    if (node.children) {
+      const result = findById(node.children, nodeId)
+      if (result) {
+        return result
+      }
+    }
   }
   return null
 }
