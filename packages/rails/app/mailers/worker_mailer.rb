6462bfe23f0e9a7f4c2a3431d9f303f77e750043
Pavel Kubal <pkubal@dnanexus.com>
2024-07-04 11:24:21 +0200

    PFDA-5468 Remove unused emailing code (#2920)
    Co-authored-by: Julie Pham <35426376+nganphtgit@users.noreply.github.com>

--- packages/rails/app/mailers/worker_mailer.rb
+++ packages/rails/app/mailers/worker_mailer.rb
@@ -15,14 +15,6 @@ class WorkerMailer < ApplicationMailer
     mail(to: email, subject: @subject)
   end
 
-  def remove_node_worker_email(email, space, error_message)
-    @space = space
-    @error_message = error_message
-
-    mail(to: email,
-         subject: "An error occurred during the removing of files in space '#{space.title}'.")
-  end
-
   def alert_email(email, message, subject)
     @message = message
     @subject = subject
