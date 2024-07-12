f7c2e482699d00ea351d3c4b9a6c35ad7c886835
Jiří Lagan <43816355+jirkalagan@users.noreply.github.com>
2024-07-12 11:10:37 +0200

    PFDA-5499 pfda team section removed (frontend) (#2935)

--- packages/client/src/features/overview/OverviewAuthed.tsx
+++ packages/client/src/features/overview/OverviewAuthed.tsx
@@ -25,14 +25,12 @@ import { IApp } from '../apps/apps.types'
 import { ChallengesBanner } from './ChallengesBanner'
 import ChallengesOverviewList from './ChallengesOverviewList'
 import { OverviewNewsList } from './OverviewNewsList'
-import { ParticipantPersonsList } from './ParticipantPersonsList'
 import { ParticipantOrgsList } from './ParticipantsOrgsList'
 import {
   CommunityParticipants,
   ExpertSection,
   Hr,
   InfoRow,
-  PFDATeamSection,
   PageOverviewMainBody,
 } from './styles'
 import { AppTypeIconBlue } from '../../components/icons/AppTypeIconBlue'
@@ -295,13 +293,6 @@ export const OverviewAuthed = () => {
           <a href="mailto:precisionfda@fda.hhs.gov">Feedback</a>
         </InfoRow>
       </PageMainBody>
-
-      <PFDATeamSection>
-        <SectionTitle>PRECISIONFDA TEAM</SectionTitle>
-        <div>
-          <ParticipantPersonsList/>
-        </div>
-      </PFDATeamSection>
     </PublicLayout>
   )
 }
