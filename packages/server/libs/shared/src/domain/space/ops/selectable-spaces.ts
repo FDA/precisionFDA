511be347228ec83d070c676dac7a61033e8ce221
Pavel Kubal <pkubal@dnanexus.com>
2024-07-15 09:19:38 +0200

    PFDA-5480 Spaces - Cannot select current space as scope when running Apps/Wfs in Admin space type (#2929)
    Co-authored-by: Jiří Lagan <43816355+jirkalagan@users.noreply.github.com>

--- packages/server/libs/shared/src/domain/space/ops/selectable-spaces.ts
+++ packages/server/libs/shared/src/domain/space/ops/selectable-spaces.ts
@@ -7,28 +7,15 @@ import { SPACE_TYPE } from '../../space/space.enum'
  * Operation checks if given space has confidential spaces and returns info
  * about corresponding space (plus potential confidential spaces).
  */
-export class SelectableSpacesOperation extends BaseOperation<
-UserOpsCtx,
-number,
-Space[]
-> {
+export class SelectableSpacesOperation extends BaseOperation<UserOpsCtx, number, Space[]> {
   async run(id: number): Promise<Space[]> {
     const em = this.ctx.em
 
     const space = await em.findOneOrFail(Space, { id })
 
-    // is the space of correct type?
-    if (!(space.type === SPACE_TYPE.REVIEW
-      || space.type === SPACE_TYPE.VERIFICATION
-      || space.type === SPACE_TYPE.GROUPS
-      || space.type === SPACE_TYPE.GOVERNMENT
-      || space.type === SPACE_TYPE.PRIVATE_TYPE)) {
-      return []
-    }
-
     // collect ids of confidential spaces + space id
     const confidentialSpaces = await em.find(Space, { spaceId: space.id })
-    const spaceIds = confidentialSpaces.map(space => space.id)
+    const spaceIds = confidentialSpaces.map((space) => space.id)
     spaceIds.push(space.id)
 
     const spaceRepo = em.getRepository(Space)
