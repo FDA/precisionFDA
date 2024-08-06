c79b5f97b9ea4f0f4771d1b67558010a98215eaf
“Ludvik <lbobek@dnanexus.com>
2024-08-06 22:13:56 +0200

    Revert "PFDA-5408: SSO redirect (#2939)"
    This reverts commit 15821b9c

--- packages/client/src/features/home/ActionDropdownContent.tsx
+++ packages/client/src/features/home/ActionDropdownContent.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
 import { CloudResourcesConditionalAnchor } from '../../components/ConditionalAnchor'
-import { CheckIcon } from '../../components/icons/CheckIcon'
-import { NavLink } from '../../components/NavLink'
 import { CloudResourcesConditionType } from '../../hooks/useCloudResourcesCondition'
 import { colors } from '../../styles/theme'
 import { ActionFunctionsType, ActionGroupType, Link as LinkType } from './types'
+import { CheckIcon } from '../../components/icons/CheckIcon'
+import { NavLink } from '../../components/NavLink'
+import MagicLink from './MagicPostLink'
 
 // Updated disbaled text color for remediation using textMediumGrey
-export const StyledActionItem = styled.li<{ disabled?: boolean; selected?: boolean }>`
+export const StyledActionItem = styled.li<{ disabled?: boolean, selected?: boolean }>`
   /* padding: 0 16px; */
   margin: 0;
   list-style: none;
@@ -31,9 +32,7 @@ export const StyledActionItem = styled.li<{ disabled?: boolean; selected?: boole
         color: var(--c-text-500);
       }
     `}
-  ${({ selected }) =>
-    selected &&
-    css`
+  ${({ selected }) => selected && css`
       font-weight: 600;
     `}
   &:hover {
@@ -101,7 +100,7 @@ export const GroupActionMenu = styled.ul`
   min-width: 170px;
 
   ${StyleSelection} {
-    padding: 4px 30px 4px 0;
+    padding: 0 30px 0 0;
   }
   ${StyledActionItem} {
     padding: 0 12px;
@@ -157,12 +156,21 @@ const ActionItem = ({ action }: { action: ActionFunctionsType<any>[number] }) =>
     case 'route':
       return (
         <StyledActionItem key={action.key} disabled={isDisabled}>
-          {isDisabled ? <span>{action.key}</span> : <NavLink to={action?.to}>{action.key}</NavLink>}
+          {isDisabled ? (
+            <span>{action.key}</span>
+          ) : (
+            <NavLink to={action?.to}>
+              {action.key}
+            </NavLink>
+          )}
         </StyledActionItem>
       )
     case 'link':
       return (
-        <StyledActionItem key={action.key} disabled={action?.isDisabled ?? true}>
+        <StyledActionItem
+          key={action.key}
+          disabled={action?.isDisabled ?? true}
+        >
           <LinkAction
             disabled={action?.isDisabled}
             link={action?.link}
@@ -174,19 +182,20 @@ const ActionItem = ({ action }: { action: ActionFunctionsType<any>[number] }) =>
       )
     case 'selection':
       return (
-        <StyledActionItem
-          key={action?.key}
+        <StyledActionItem key={action?.key}
           onClick={() => {
             if (!action?.isDisabled) {
               action?.func(!action.isSelected)
             }
           }}
           disabled={action?.isDisabled ?? true}
-          selected={action.isSelected}
+          selected={ action.isSelected }
         >
           <StyleSelection>
-            <StyleSelectionIcon>{action.isSelected && <CheckIcon color={colors.primaryBlue} height={12} />}</StyleSelectionIcon>
-            {action?.content}
+            <StyleSelectionIcon>
+              {action.isSelected && <CheckIcon color={colors.primaryBlue} height={12} />}
+            </StyleSelectionIcon>
+            {action?.title}
           </StyleSelection>
         </StyledActionItem>
       )
@@ -204,7 +213,13 @@ const ActionItem = ({ action }: { action: ActionFunctionsType<any>[number] }) =>
   }
 }
 
-export function ActionsDropdownContent({ actions, message }: { actions: ActionFunctionsType<any>; message?: React.ReactNode }) {
+export function ActionsDropdownContent({
+  actions,
+  message,
+}: {
+  actions: ActionFunctionsType<any>
+  message?: React.ReactNode
+}) {
   const visibleActions = Object.keys(actions)
     .filter(a => !actions[a]?.shouldHide ?? true)
     .map(v => ({ key: v, ...actions[v] }))
@@ -219,7 +234,11 @@ export function ActionsDropdownContent({ actions, message }: { actions: ActionFu
   )
 }
 
-export function ActionsDropdownGroupContent({ content }: { content: ActionGroupType[] }) {
+export function ActionsDropdownGroupContent({
+  content,
+}: {
+  content: ActionGroupType[]
+}) {
   return (
     <GroupActionMenu>
       {content.map((item, index) => {
