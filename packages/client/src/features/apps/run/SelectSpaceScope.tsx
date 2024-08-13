5e16dc238d03595298c2839429e5ef390ecde723
Pavel Kubal <pkubal@dnanexus.com>
2024-08-13 16:13:26 +0200

    PFDA-5569 Spaces - Group space - Run WF input is not showing the full name on the space (#2987)

--- packages/client/src/features/apps/run/SelectSpaceScope.tsx
+++ packages/client/src/features/apps/run/SelectSpaceScope.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 import { FieldGroup } from '../../../components/form/FieldGroup'
 import { Select } from '../../../components/Select'
 import { ErrorMessageForField } from './ErrorMessageForField'
-import { StyledScopeName } from './styles'
 import { RunJobFormType, SelectableSpace } from '../apps.types'
+import { RunWorkflowFormType } from '../../workflows/run/RunWorkflowForm'
 
 /**
  * Component for selecting space scope if applicable.
@@ -16,39 +16,38 @@ import { RunJobFormType, SelectableSpace } from '../apps.types'
  * @constructor
  */
 export const SelectSpaceScope = ({
-                                   control,
-                                   isSubmitting,
-                                   selectableSpaces,
-                                   errors,
-                                 }: {
-  control: Control<RunJobFormType>
+  control,
+  isSubmitting,
+  selectableSpaces,
+  errors,
+}: {
+  control: Control<RunJobFormType | RunWorkflowFormType>
   isSubmitting: boolean
   selectableSpaces: SelectableSpace[] | undefined
-  errors: FieldErrors<RunJobFormType>
+  errors: FieldErrors<RunJobFormType | RunWorkflowFormType>
 }) => {
-  return <StyledScopeName>
-    <FieldGroup label="Space scope" required>
-      <Controller
-        name="scope"
-        control={control}
-        render={({ field }) => (
-          <Select
-            options={selectableSpaces}
-            placeholder="Choose..."
-            onChange={value => {
-              field.onChange(value)
-              field.onBlur()
-            }}
-            isClearable
-            isSearchable
-            onBlur={field.onBlur}
-            value={field.value}
-            isDisabled={isSubmitting}
-            inputId="select_context"
-          />
-        )}
-      />
-      <ErrorMessageForField errors={errors as FieldErrors<Record<string, unknown>>} fieldName="scope" />
-    </FieldGroup>
-  </StyledScopeName> 
+  return (
+  <FieldGroup label="Space scope" required>
+    <Controller
+      name="scope"
+      control={control}
+      render={({ field }) => (
+        <Select
+          options={selectableSpaces}
+          placeholder="Choose..."
+          onChange={value => {
+            field.onChange(value)
+            field.onBlur()
+          }}
+          isClearable
+          isSearchable
+          onBlur={field.onBlur}
+          value={field.value}
+          isDisabled={isSubmitting}
+          inputId="select_context"
+        />
+      )}
+    />
+    <ErrorMessageForField errors={errors as FieldErrors<Record<string, unknown>>} fieldName="scope" />
+  </FieldGroup>)
 }
