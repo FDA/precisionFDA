b7b2b0823454437704566154d9da7ee80be7b26b
Ludvik Bobek <139764338+lbobek-dnanexus@users.noreply.github.com>
2024-10-24 11:28:59 +0200

    PFDA-5761 - expired session (#3087)

--- packages/server/apps/api/src/cli/cli.controller.ts
+++ packages/server/apps/api/src/cli/cli.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common'
+import { Body, Controller, Get, HttpCode, Param, Post, UseGuards } from '@nestjs/common'
 import { CliNodeSearchDTO } from '@shared/domain/cli/dto/CliNodeSearchDTO'
 import { CliService } from '@shared/domain/cli/service/cli.service'
 import { DxId } from '@shared/domain/entity/domain/dxid'
+import { UserContextGuard } from '../user-context/guard/user-context.guard'
 
 // SPECIAL ROUTES INTENDED FOR CLI USAGE ONLY. CONTAINS CLI SPECIFIC LOGIC & SPECIAL RESPONSE OBJECTS.
 @Controller('/cli')
 export class CliController {
   constructor(private readonly cliService: CliService) {}
 
+  @UseGuards(UserContextGuard)
   @HttpCode(200)
   @Post('/nodes')
   async findNodes(@Body() body: CliNodeSearchDTO) {
@@ -19,26 +21,31 @@ export class CliController {
     return { version: '2.7.2' }
   }
 
+  @UseGuards(UserContextGuard)
   @Get('/:uid/describe')
   async describeEntity(@Param('uid') uid: string) {
     return this.cliService.describeEntity(uid)
   }
 
+  @UseGuards(UserContextGuard)
   @Get('/job/:dxid/scope')
   async getJobScope(@Param('dxid') jobDxid: DxId<'job'>) {
     return this.cliService.getJobScope(jobDxid)
   }
 
+  @UseGuards(UserContextGuard)
   @Get('/spaces/:id/members')
   async listMembers(@Param('id') spaceId: number) {
     return this.cliService.listSpaceMembers(spaceId)
   }
 
+  @UseGuards(UserContextGuard)
   @Get('/spaces/:id/discussions')
   async listDiscussions(@Param('id') spaceId: number) {
     return this.cliService.listSpaceDiscussions(spaceId)
   }
 
+  @UseGuards(UserContextGuard)
   @Get('/discussions/:discussionId/describe')
   async describeDiscussion(@Param('discussionId') discussionId: number) {
     return this.cliService.describeDiscussion(discussionId)
