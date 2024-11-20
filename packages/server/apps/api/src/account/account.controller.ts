c658cfe13364347eea84236424c1bab00d5706fa
Ludvik Bobek <139764338+lbobek-dnanexus@users.noreply.github.com>
2024-11-20 15:49:50 +0100

    PFDA-5831 - Add: internal endpoint guard (#3132)
    Co-authored-by: “Ludvik <“lbobek@dnanexus.com”>

--- packages/server/apps/api/src/account/account.controller.ts
+++ packages/server/apps/api/src/account/account.controller.ts
@@ -2,9 +2,10 @@ import { TASK_TYPE } from '@shared/queue/task.input'
 import { Controller, Get, HttpCode, Post, UseGuards } from '@nestjs/common'
 import { createSyncSpacesPermissionsTask, createUserCheckupTask } from '@shared/queue'
 import { UserContext } from '@shared/domain/user-context/model/user-context'
+import { InternalRouteGuard } from '../internal/guard/internal.guard'
 import { UserContextGuard } from '../user-context/guard/user-context.guard'
 
-@UseGuards(UserContextGuard)
+@UseGuards(InternalRouteGuard, UserContextGuard)
 @Controller('/account')
 export class AccountController {
   constructor(private readonly user: UserContext) {}
