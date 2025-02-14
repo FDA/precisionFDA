bba10895ab14df4ccd8d23d1ba4e6e7ab557c639
Pavel Kubal <pkubal@dnanexus.com>
2025-02-14 12:38:02 +0100

    PFDA-6054 Action - Move in space is missing confirmation notification (#3250)

--- packages/client/src/features/files/useFilesSelectActions.tsx
+++ packages/client/src/features/files/useFilesSelectActions.tsx
@@ -238,10 +238,15 @@ export const useFilesSelectActions = ({
     headerText: `Move ${selected.length} item${selected.length === 1 ? '' : 's'}`,
     submitCaption: 'Move',
     scope: getFileScope(homeScope, space),
-    onHandleSubmit: selectedFolderId => {
-      moveFilesMutation.mutateAsync(selectedFolderId).then(() => {
-        setMoveFileModal(false)
-      })
+    onHandleSubmit: (selectedFolderId: number, info: TreeOnSelectInfo) => {
+      moveFilesMutation.mutateAsync(selectedFolderId)
+        .then(() => {
+          setMoveFileModal(false)
+          toast.success(`Successfully moved ${selected.length} ${pluralize('item', selected.length)} to ${info.node.title}`)
+        })
+        .catch((error) => {
+          toast.error(`Error moving files: ${error}`)
+        })
     },
   })
   const sourceScopes = new Set()
