0ab11c5f4cdb8f5e89cfb3084a2e49a423a537fc
“Ludvik <lbobek@dnanexus.com>
2025-03-31 10:37:25 +0200

    PFDA-6177 - Mod: custom repository user context injection

--- packages/server/libs/shared/src/domain/discussion/discussion.repository.ts
+++ packages/server/libs/shared/src/domain/discussion/discussion.repository.ts
@@ -1,22 +1,12 @@
-import { Discussion } from '@shared/domain/discussion/discussion.entity'
-import { AccessControlRepository } from '@shared/repository/access-control.repository'
 import { FilterQuery } from '@mikro-orm/core'
-import { STATIC_SCOPE } from '@shared/enums'
-import { SqlEntityManager } from '@mikro-orm/mysql'
-import { UserContext } from '@shared/domain/user-context/model/user-context'
-import { User } from '@shared/domain/user/user.entity'
 import { Injectable } from '@nestjs/common'
+import { Discussion } from '@shared/domain/discussion/discussion.entity'
+import { User } from '@shared/domain/user/user.entity'
+import { STATIC_SCOPE } from '@shared/enums'
+import { AccessControlRepository } from '@shared/repository/access-control.repository'
 
 @Injectable()
 export default class DiscussionRepository extends AccessControlRepository<Discussion> {
-  constructor(
-    em: SqlEntityManager,
-    private readonly user: UserContext, // need also the user db record (for accessible spaces)
-  ) {
-    // TODO XXXX_LUDVIK
-    super(em, Discussion)
-  }
-
   protected async getAccessibleWhere(): Promise<FilterQuery<Discussion>> {
     const user = await this.em.findOneOrFail(User, { id: this.user.id })
     // const user = await this.em.findOneOrFail(User, { id: 7 })
