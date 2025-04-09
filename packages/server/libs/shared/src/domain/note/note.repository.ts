dd49503a77954b519a52c3f627ca86eaac8808ea
Jiri Lagan <jlagan@dnanexus.com>
2025-04-09 14:26:31 +0200

    implement rules for comparisons and notes

--- packages/server/libs/shared/src/domain/note/note.repository.ts
+++ packages/server/libs/shared/src/domain/note/note.repository.ts
@@ -1,13 +1,45 @@
 import { AccessControlRepository } from '@shared/repository/access-control.repository'
 import { Note } from '@shared/domain/note/note.entity'
 import { FilterQuery } from '@mikro-orm/core'
+import { User } from '@shared/domain/user/user.entity'
+import { STATIC_SCOPE } from '@shared/enums'
 
 export class NoteRepository extends AccessControlRepository<Note> {
-  protected getAccessibleWhere(): Promise<FilterQuery<Note>> {
-    throw new Error('Method not implemented.')
+  protected async getAccessibleWhere(): Promise<FilterQuery<Note>> {
+    const user = await this.em.findOneOrFail(User, { id: this.user.id })
+    const accessibleSpaces = await user.accessibleSpaces()
+    const scopes = accessibleSpaces.map((space) => space.scope)
+
+    const isSiteAdmin = await user.isSiteAdmin()
+    if (isSiteAdmin) {
+      return {}
+    }
+
+    return {
+      $or: [
+        { scope: STATIC_SCOPE.PUBLIC },
+        { scope: { $in: scopes } },
+        { user: user.id, scope: STATIC_SCOPE.PRIVATE },
+      ],
+    }
   }
 
-  protected getEditableWhere(): Promise<FilterQuery<Note>> {
-    throw new Error('Method not implemented.')
+  protected async getEditableWhere(): Promise<FilterQuery<Note>> {
+    const user = await this.em.findOneOrFail(User, { id: this.user.id })
+    const editableSpaces = await user.editableSpaces()
+    const scopes = editableSpaces.map((space) => space.scope)
+
+    const isSiteAdmin = await user.isSiteAdmin()
+    if (isSiteAdmin) {
+      return {}
+    }
+
+    return {
+      $or: [
+        { user: user.id, scope: STATIC_SCOPE.PUBLIC },
+        { user: user.id, scope: STATIC_SCOPE.PRIVATE },
+        { scope: { $in: scopes } },
+      ],
+    }
   }
 }
