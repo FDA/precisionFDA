16fa33b6aef1bde5dbe84e0583a59889888d44b4
Jiri Lagan <jlagan@dnanexus.com>
2025-04-09 14:26:31 +0200

    implement rules for comparisons and notes

--- packages/server/libs/shared/src/domain/comparison/comparison.repository.ts
+++ packages/server/libs/shared/src/domain/comparison/comparison.repository.ts
@@ -2,14 +2,29 @@ import { FilterQuery } from '@mikro-orm/core'
 import { Comparison } from '@shared/domain/comparison/comparison.entity'
 import { UserFile } from '@shared/domain/user-file/user-file.entity'
 import { AccessControlRepository } from '@shared/repository/access-control.repository'
+import { User } from '@shared/domain/user/user.entity'
+import { STATIC_SCOPE } from '@shared/enums'
 
 export class ComparisonRepository extends AccessControlRepository<Comparison> {
-  protected getAccessibleWhere(): Promise<FilterQuery<Comparison>> {
-    throw new Error('Method not implemented.')
+  protected async getAccessibleWhere(): Promise<FilterQuery<Comparison>> {
+    const user = await this.em.findOneOrFail(User, { id: this.user.id })
+
+    return {
+      $or: [{ user: user.id, scope: STATIC_SCOPE.PRIVATE }, { scope: STATIC_SCOPE.PUBLIC }],
+    }
   }
 
-  protected getEditableWhere(): Promise<FilterQuery<Comparison>> {
-    throw new Error('Method not implemented.')
+  protected async getEditableWhere(): Promise<FilterQuery<Comparison>> {
+    const user = await this.em.findOneOrFail(User, { id: this.user.id })
+
+    // TODO: define rules for site-admins
+
+    return {
+      $or: [
+        { user: user.id, scope: STATIC_SCOPE.PRIVATE },
+        { user: user.id, scope: STATIC_SCOPE.PUBLIC },
+      ],
+    }
   }
   async findComparisonsByUserFile(userFile: UserFile) {
     return await this.find({ inputFiles: [userFile] })
