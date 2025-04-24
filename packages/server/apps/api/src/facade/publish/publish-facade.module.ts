9df1e7ab1cb1c8edaf5f6ffa4293d09d4c0f7ac6
Jiri Lagan <jlagan@dnanexus.com>
2025-04-24 07:57:29 +0200

    publish feature fix

--- packages/server/apps/api/src/facade/publish/publish-facade.module.ts
+++ packages/server/apps/api/src/facade/publish/publish-facade.module.ts
@@ -6,9 +6,18 @@ import { PublishApiFacade } from './publish.facade'
 import { AppModule } from '@shared/domain/app/app.module'
 import { JobModule } from '@shared/domain/job/job.module'
 import { NoteModule } from '@shared/domain/note/note.module'
+import { ComparisonModule } from '@shared/domain/comparison/comparison.module'
 
 @Module({
-  imports: [EntityProvenanceModule, EntityModule, UserFileModule, AppModule, JobModule, NoteModule],
+  imports: [
+    EntityProvenanceModule,
+    EntityModule,
+    UserFileModule,
+    AppModule,
+    JobModule,
+    NoteModule,
+    ComparisonModule,
+  ],
   providers: [PublishApiFacade],
   exports: [PublishApiFacade],
 })
