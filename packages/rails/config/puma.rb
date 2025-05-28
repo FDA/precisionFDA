e1a0b555abfc724c5685ba3b12ade3a0242b5649
Pavel Kubal <pkubal@dnanexus.com>
2025-05-28 16:05:15 +0200

    PFDA-5683 Fix DB pool settings (#3437)

--- packages/rails/config/puma.rb
+++ packages/rails/config/puma.rb
@@ -1,7 +1,7 @@
 environment ENV["RAILS_ENV"] || "production"
-threads 2, 16
-port 3000
-workers ENV.fetch("WEB_CONCURRENCY", 4).to_i
+threads    2, 16
+port       3000
+workers    ENV.fetch("WEB_CONCURRENCY", 4).to_i
 stdout_redirect File.expand_path("../../../log/puma.log", __dir__),
                 File.expand_path("../../../log/puma-err.log", __dir__)
 
@@ -9,14 +9,15 @@ preload_app!
 
 before_fork do
   Auditor.init!
-
   ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
 end
 
-after_worker_fork do
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection(
-      ENV.fetch("DATABASE_URL", Rails.application.config.database_configuration[Rails.env]),
-    )
-  end
+on_worker_boot do
+  # disconnect any inherited connections
+  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord::Base)
+
+  # re‑establish using YAML settings
+  ActiveRecord::Base.establish_connection
+
+  Rails.logger.info "Puma worker #{Process.pid} DB pool size = #{ActiveRecord::Base.connection_pool.size}"
 end
