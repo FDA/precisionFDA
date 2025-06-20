348a903530ed742f4e81ea3a382ae2f746537012
Dave <dcech@dnanexus.com>
2025-06-20 13:00:32 +0200

    PFDA-6373 - upgrade to storybook v9 (#3464)

--- packages/client/eslint.config.mjs
+++ packages/client/eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook'
+
 // @ts-check
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
@@ -54,4 +57,5 @@ export default tseslint.config(
   {
     ignores: ['src/features/lexi/**/*'],
   },
+  storybook.configs['flat/recommended'],
 )
