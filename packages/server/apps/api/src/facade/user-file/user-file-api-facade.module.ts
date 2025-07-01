bcefc7edc589bfcbaacc4d8c21b4d1a6ef85ca81
Pavel Kubal <pkubal@dnanexus.com>
2025-07-01 13:37:31 +0200

    PFDA-6347 get rid of entityrepotype (#3454)
    Co-authored-by: Julie Pham <35426376+nganphtgit@users.noreply.github.com>
    Co-authored-by: John Varghese <jvarghese@dnanexus.com>
    Co-authored-by: shifaizM <52531296+shifaizM@users.noreply.github.com>
    Co-authored-by: Dave <dcech@dnanexus.com>

--- packages/server/apps/api/src/facade/user-file/user-file-api-facade.module.ts
+++ packages/server/apps/api/src/facade/user-file/user-file-api-facade.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { UserFileResolverFacade } from './user-file-resolver.facade'
+import { MikroOrmModule } from '@mikro-orm/nestjs'
+import { UserFile } from '@shared/domain/user-file/user-file.entity'
 
 @Module({
-  imports: [],
+  imports: [MikroOrmModule.forFeature([UserFile])],
   providers: [UserFileResolverFacade],
   exports: [UserFileResolverFacade],
 })
