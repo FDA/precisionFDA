d28a8d247ff15bcc3bb557fb98200058ae0a3b64
Pavel Kubal <pkubal@dnanexus.com>
2025-07-03 12:36:01 +0200

    PFDA-6409 added formatting

--- packages/client/src/components/CloudResourcesModal.tsx
+++ packages/client/src/components/CloudResourcesModal.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
 import { Loader } from './Loader'
-import { colors, theme } from '../styles/theme'
+import { colors } from '../styles/theme'
 import { CloudResourcesResponse, useCloudResourcesQuery } from '../hooks/useCloudResourcesCondition'
 import { ModalHeaderTop, ModalNext } from '../features/modal/ModalNext'
+
+const formatWithSpaces = (value: number): string => {
+  return value
+    .toFixed(2)
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+}
 
 type Props = {
   isShown: boolean
@@ -106,7 +112,7 @@ const CloudUsageReport = ({ stats }: CloudUsageReportProps) => {
             <StyledBodyCell $bold={row.bold} $textAlign='right' $bgColor={
               index % 2 === 0 ? colors.borderDefault : colors.backgroundLightGray
             }>
-              {`$${row.usage.toFixed(2)}`}
+              {`$${formatWithSpaces(row.usage)}`}
             </StyledBodyCell>
           </StyledTr>
         ))}
