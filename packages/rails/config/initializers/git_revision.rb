22b27bd4ddf98e923964ffdb5a930cfdd9a17d2d
shifaizM <52531296+shifaizM@users.noreply.github.com>
2025-11-14 14:35:31 +0100

    PFDA-6621: fix revision not showing up (#3658)

--- packages/rails/config/initializers/git_revision.rb
+++ packages/rails/config/initializers/git_revision.rb
@@ -1,2 +1,3 @@
-GIT_REVISION = `git rev-parse HEAD`.strip rescue 'N/A'
-GIT_BRANCH = `git rev-parse --abbrev-ref HEAD`.strip rescue 'N/A'
+# Get the actual branch (from ENV if set)
+GIT_REVISION = ENV["GIT_REVISION"].presence || `git rev-parse HEAD`.strip rescue "N/A"
+GIT_BRANCH = ENV["GIT_BRANCH"].presence || `git rev-parse --abbrev-ref HEAD`.strip rescue "N/A"
