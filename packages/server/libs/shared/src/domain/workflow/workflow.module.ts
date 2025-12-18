c59ee9298d23932810df919821e0c15f59a01afb
Pavel Kubal <pkubal@dnanexus.com>
2025-12-18 12:48:38 +0100

    PFDA-6676 added WorkflowSeries to imports (#3705)
    Co-authored-by: Jiří Lagan <43816355+jirkalagan@users.noreply.github.com>

--- packages/server/libs/shared/src/domain/workflow/workflow.module.ts
+++ packages/server/libs/shared/src/domain/workflow/workflow.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { WorkflowService } from '@shared/domain/workflow/service/workflow.service'
 import { MikroOrmModule } from '@mikro-orm/nestjs'
 import { Workflow } from '@shared/domain/workflow/entity/workflow.entity'
+import { WorkflowSeries } from '@shared/domain/workflow-series/workflow-series.entity'
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Workflow])],
+  imports: [MikroOrmModule.forFeature([Workflow, WorkflowSeries])],
   providers: [WorkflowService],
   exports: [WorkflowService, MikroOrmModule],
 })
