c982e825f04f29c5fce8dfcf872c8097b2f319b6
shifaizM <52531296+shifaizM@users.noreply.github.com>
2025-12-22 06:20:39 +0100

    PFDA-6669: fix uni link and PFDA-6662: Fix recaptcha issue (#3694)
    Co-authored-by: Pavel Kubal <pkubal@dnanexus.com>

--- packages/nginx/entrypoint.sh
+++ packages/nginx/entrypoint.sh
@@ -5,5 +5,7 @@ set -e
 echo "$SSL_CERT" > /etc/nginx/ssl/pfda.crt
 echo "$SSL_KEY" > /etc/nginx/ssl/pfda.key
 
+sed -i "s|\${UNII_HOST}|$UNII_HOST|g" /etc/nginx/nginx.conf
+
 # Execute CMD arguments (nginx -g 'daemon off;')
 exec "$@"
