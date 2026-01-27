8f9bd7f932e15f5070e43f0f8ebaac6cd7241725
Julie Pham <35426376+nganphtgit@users.noreply.github.com>
2026-01-27 16:26:58 +0700

    PFDA-6728 - fix: blank app details page (#3749)
    Co-authored-by: Jiří Lagan <43816355+jirkalagan@users.noreply.github.com>

--- packages/client/src/features/apps/SpecTab/index.tsx
+++ packages/client/src/features/apps/SpecTab/index.tsx
@@ -1,29 +1,27 @@
 import React from 'react'
+import { RESOURCE_LABELS } from '../../../types/user'
+import { MetadataKey } from '../../home/show.styles'
+import { AppSpec } from '../apps.types'
 import { SpecTable } from './SpecTable'
 import { StyledSpecTab } from './styles'
-import { AppSpec } from '../apps.types'
-import { MetadataKey } from '../../home/show.styles'
-import { RESOURCE_LABELS } from '../../../types/user'
 
+export const SpecTab = ({ spec, spaceId }: { spec: AppSpec; spaceId?: string }): React.JSX.Element => {
+  if (!spec) {
+    return <></>
+  }
 
-export const SpecTab = ({ spec, spaceId }: { spec: AppSpec, spaceId?: string }) => {
   const internetAccess = spec.internet_access ? 'Yes' : 'No'
-
   return (
     <StyledSpecTab>
       <div className="__header">
         <div className="__header_item">
-          <MetadataKey>
-            default instance type
-          </MetadataKey>
+          <MetadataKey>default instance type</MetadataKey>
           <div data-testid="app-default-instance-type" className="__header_item_value">
-            {RESOURCE_LABELS[spec.instance_type] ?? spec.instance_type}
+            {RESOURCE_LABELS[spec.instance_type as keyof typeof RESOURCE_LABELS] ?? spec.instance_type}
           </div>
         </div>
         <div className="__header_item">
-          <MetadataKey>
-            has internet access
-          </MetadataKey>
+          <MetadataKey>has internet access</MetadataKey>
           <div data-testid="app-has-internet-access" className="__header_item_value">
             {internetAccess}
           </div>
