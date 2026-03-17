1e37067e884ce33f587664b8fa5c6c698e0a12c1
Julie Pham <35426376+nganphtgit@users.noreply.github.com>
2026-03-17 20:58:58 +0700

    PFDA-6821 - fix: skip failure org for membership update in Group spaces (#3841)

--- packages/server/libs/shared/src/domain/space-membership/service/update-permission/space-membership-update-permission.provider.ts
+++ packages/server/libs/shared/src/domain/space-membership/service/update-permission/space-membership-update-permission.provider.ts
@@ -1,14 +1,20 @@
 import { SqlEntityManager } from '@mikro-orm/mysql'
+import { Logger } from '@nestjs/common'
 import { DxId } from '@shared/domain/entity/domain/dxid'
 import { SpaceMembershipPlatformAccessProvider } from '@shared/domain/space-membership/providers/platform-access/space-membership-platform-access.provider'
 import { SpaceMembership } from '@shared/domain/space-membership/space-membership.entity'
 import { SPACE_MEMBERSHIP_ROLE } from '@shared/domain/space-membership/space-membership.enum'
 import { SpaceMembershipRepository } from '@shared/domain/space-membership/space-membership.repository'
 import { Space } from '@shared/domain/space/space.entity'
-import { PermissionError } from '@shared/errors'
+import { SPACE_TYPE } from '@shared/domain/space/space.enum'
+import { BaseError, PermissionError } from '@shared/errors'
+import { ServiceLogger } from '@shared/logger/decorator/service-logger'
 import { PlatformClient } from '@shared/platform-client'
 
 export abstract class SpaceMembershipUpdatePermissionProvider {
+  @ServiceLogger()
+  logger: Logger
+
   protected constructor(
     protected readonly em: SqlEntityManager,
     protected readonly platformClient: PlatformClient,
@@ -21,9 +27,7 @@ export abstract class SpaceMembershipUpdatePermissionProvider {
 
   async validateUpdaterRole(currentMembership: SpaceMembership): Promise<void> {
     if (!this.permittedUpdaterRoles.includes(currentMembership.role)) {
-      throw new PermissionError(
-        'Current user does not have permission to update memberships to target role',
-      )
+      throw new PermissionError('Current user does not have permission to update memberships to target role')
     }
   }
 
@@ -43,8 +47,23 @@ export abstract class SpaceMembershipUpdatePermissionProvider {
     changeableMemberships: SpaceMembership[],
   ): Promise<void> {
     const orgs = space.getMembershipOrg(membership)
-    const promises = orgs.map((org) => {
-      return this.updateOrgAccess(org, changeableMemberships)
+    const promises = orgs.map(org => {
+      return this.updateOrgAccess(org, changeableMemberships).catch((err: BaseError) => {
+        if (
+          space.type === SPACE_TYPE.GROUPS &&
+          err.message === `PermissionDenied (401): Administrator access to ${org} required to perform this operation`
+        ) {
+          // Log the error and continue with other orgs
+          this.logger.error(`Failed to update platform access for org ${org} and space ${space.id}: ${err.message}`)
+          this.logger.error(
+            'This happens for legacy orgs where the leads were not invited to the reverse org when the org was created',
+          )
+          // TODO(PFDA-6821): create task to sync access for failure org by utilizing admin token
+
+          return
+        }
+        throw err
+      })
     })
     await Promise.all(promises)
   }
