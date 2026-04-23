c2b5473c743ab98b71d121734be9f7c2fb16aacc
Jiří Lagan <43816355+jirkalagan@users.noreply.github.com>
2026-04-23 14:03:49 +0200

    PFDA-6705 pending users filtering + fixes (#3909)

--- packages/client/src/features/admin/pendingUsers/api.ts
+++ packages/client/src/features/admin/pendingUsers/api.ts
@@ -1,7 +1,15 @@
 import axios from 'axios'
-import { IFilter } from '../../home/types'
-import { Params, prepareListFetchV2 } from '../../home/utils'
-import { AdminUserListType } from '../users/types'
+import type { IFilter, MetaV2 } from '../../home/types'
+import { type Params, prepareListFetchV2 } from '../../home/utils'
+
+export type PendingUser = {
+  id: number
+  dxuser: string
+  email: string
+  createdAt: string
+}
+
+export type PendingUserListType = { data: PendingUser[]; meta: MetaV2 }
 
 export const resendActivationEmail = async (userId: number): Promise<void> => {
   return await axios.post(`/api/v2/admin/users/${userId}/resend-activation-email`).then(res => res.data)
@@ -9,5 +17,5 @@ export const resendActivationEmail = async (userId: number): Promise<void> => {
 
 export async function fetchPendingUsers(filters: IFilter[], params: Params) {
   const query = prepareListFetchV2(filters, params)
-  return axios.get<AdminUserListType>('/api/v2/admin/users/pending', { params: query }).then(res => res.data)
+  return axios.get<PendingUserListType>('/api/v2/admin/users/pending', { params: query }).then(res => res.data)
 }
